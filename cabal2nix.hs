19babcb14ea83697cf6562983fb11ab50c081320
Peter Simons <simons@cryp.to>
2011-08-05 18:31:57 +0200

    cabal2nix.hs: adapted toNixName function to coding guidelines

--- cabal2nix.hs
+++ cabal2nix.hs
@@ -1,6 +1,14 @@
+-- cabal2nix.hs
+--
+-- Copyright (c) 20011 Peter Simons <simons@cryp.to>
+-- See LICENSE file for licensing details.
+
 module Main ( main ) where
 
+import System.IO
 import System.Environment
+import Control.Exception
+import System.Exit
 import Distribution.PackageDescription.Parse
 import Distribution.Verbosity ( silent )
 import Distribution.PackageDescription
@@ -8,35 +16,53 @@ import Distribution.Package
 import Distribution.License
 import Data.Version
 import Data.List
+import Control.Monad
+import Data.Char
 
-data Pkg = Pkg String [Int] String String String License [CondTree ConfVar [Dependency] ()] [String]
+type PkgName = String
+type PkgVersion = [Int]
+type PkgSHA256 = String
+type PkgURL = String
+type PkgDescription = String
+type PkgLicense = License
+type PkgDependencies = [CondTree ConfVar [Dependency] ()]
+type PkgExtraLibs = [String]
 
-instance Show Pkg where
-  show (Pkg name ver sha256 url desc lic deps libs) =
-       "  " ++ attrib ++ " = callPackage ({cabal," ++ depArgs ++"}: cabal.mkDerivation (self : {\n"
-    ++ "    pname = " ++ show name ++ ";\n"
-    ++ "    version = \"" ++ showVer ++ "\";\n"
-    ++ "    sha256 = " ++ show sha256 ++ ";\n"
-    ++ "    propagatedBuildInputs = [" ++ depList ++ "];\n"
-    ++ "    meta = {\n"
-    ++ "      homepage = \"" ++ url ++ "\";\n"
-    ++ "      description = " ++ show desc ++ ";\n"
-    ++ "      license = " ++ showLic lic ++ ";\n"
-    ++ "      platforms = self.stdenv.lib.platforms.haskellPlatforms;\n"
-    ++ "      maintainers = [ self.stdenv.lib.maintainers.simons ];\n"
-    ++ "    };\n"
-    ++ "  })) {};\n"
+data Pkg = Pkg PkgName PkgVersion PkgSHA256 PkgURL PkgDescription PkgLicense PkgDependencies PkgExtraLibs
+  deriving (Show)
+
+toNixName :: String -> String
+toNixName name = f name
+  where
+    f []                            = []
+    f ('-':c:cs) | c `notElem` "-"  = toUpper c : f cs
+    f ('-':_)                       = error ("unexpected package name " ++ show name)
+    f (c:cs)                        = c : f cs
+
+toNix :: Pkg -> String
+toNix (Pkg name ver sha256 url desc lic deps libs) =
+       "{" ++ exprArgs ++"}:\n\n"
+    ++ "cabal.mkDerivation (self : {\n"
+    ++ "  pname = " ++ show name ++ ";\n"
+    ++ "  version = \"" ++ showVer ++ "\";\n"
+    ++ "  sha256 = " ++ show sha256 ++ ";\n"
+    ++ "  propagatedBuildInputs = [" ++ depList ++ "];\n"
+    ++ "  meta = {\n"
+    ++ "    homepage = \"" ++ url ++ "\";\n"
+    ++ "    description = " ++ show desc ++ ";\n"
+    ++ "    license = " ++ showLic lic ++ ";\n"
+    ++ "  };\n"
+    ++ "})\n"
     where
-      attrib  = concat (intersperse "_" (nixName name : (map show ver)))
+      exprArgs = concat (intersperse "," ((if "cabal" `notElem` pkgDeps then ["cabal"] else []) ++ pkgDeps))
       showVer = concat (intersperse "." (map show ver))
       depList = concat (intersperse " " pkgDeps)
-      depArgs = concat (intersperse "," pkgDeps)
       pkgDeps :: [String]
-      pkgDeps = map nixName $
-                  nub $ sort $
-                    libs ++ [ n | dep <- deps, Dependency (PackageName n) _ <- condTreeConstraints dep
-                                , n `notElem` ["base","containers"]
-                            ]
+      pkgDeps = map toNixName $
+                  nub $ sort $ libs ++
+                    [ n | dep <- deps, Dependency (PackageName n) _ <- condTreeConstraints dep
+                        , n `notElem` ["base","containers"]
+                    ]
       showLic (GPL Nothing)                     = show "GPL"
       showLic (GPL (Just (Version [2] [])))     = "self.stdenv.lib.licenses.gpl2"
       showLic (GPL (Just (Version [3] [])))     = "self.stdenv.lib.licenses.gpl3"
@@ -50,8 +76,6 @@ instance Show Pkg where
       showLic AllRightsReserved                 = "unknown"
       showLic OtherLicense                      = "unknown"
       showLic l                                 = error $ "unknown license: " ++ show l
-
-      nixName = map (\c -> if c == '-' then '_' else c)
 
 readPackage :: FilePath -> String -> IO Pkg
 readPackage cabalFile sha256 = do
@@ -68,6 +92,7 @@ readPackage cabalFile sha256 = do
       libs = concat [ extraLibs (libBuildInfo (condTreeData x)) | x <- libDeps ]
       libs' = concat [ extraLibs (buildInfo (condTreeData x)) | x <- exeDeps ]
   return (Pkg pkgname pkgver sha256 url desc lic (map simplify libDeps ++ map simplify exeDeps) (libs++libs'))
+    where
 
 simplify :: CondTree ConfVar [Dependency] a -> CondTree ConfVar [Dependency] ()
 simplify (CondNode _ deps nodes) = CondNode () deps (map simp nodes)
@@ -75,7 +100,15 @@ simplify (CondNode _ deps nodes) = CondNode () deps (map simp nodes)
     simp (cond,tree,mtree) = (cond, simplify tree, maybe Nothing (Just . simplify) mtree)
 
 main :: IO ()
-main = do
-  cabalFile:sha256:[] <- getArgs
+main = bracket (return ()) (\() -> hFlush stdout >> hFlush stderr) $ \() -> do
+  args <- getArgs
+
+  when (length args /= 2) $ do
+    mapM_ (hPutStrLn stderr) [ "*** invalid command line syntax"
+                             , "Usage: cabal2nix cabal-file sha256-hash"
+                             ]
+    exitFailure
+
+  let cabalFile:sha256:[] = args
   pkg <- readPackage cabalFile sha256
-  putStr (show pkg)
+  putStr (toNix pkg)
