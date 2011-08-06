45d21850c6f5bc4aee3dfe46f1e1fb80bd431b3b
Andres Loeh <mail@andres-loeh.de>
2011-08-06 20:55:54 +0200

    Extended the list of core packages.

--- src/Cabal2Nix/Package.hs
+++ src/Cabal2Nix/Package.hs
@@ -2,7 +2,6 @@ module Cabal2Nix.Package where
 
 import Data.List
 import Data.Maybe
-
 import Distribution.Package
 import Distribution.PackageDescription
 import Distribution.Version
@@ -50,8 +49,38 @@ toNix (Pkg name ver sha256 url desc lic deps libs) =
       pkgDeps :: [String]
       pkgDeps = filter (/="cabal") $ nub $ sort $ map toNixName $
                   libs ++ [ n | dep <- deps, Dependency (PackageName n) _ <- condTreeConstraints dep
-                              , n `notElem` ["base","containers"]
+                              , n `notElem` corePackages
                           ]
+
+-- | List of packages shipped with ghc and therefore at the moment not in
+-- nixpkgs. This should probably be configurable at first. Later, it might
+-- be good to actually include them as dependencies, but set them to null
+-- if GHC provides them (as different GHC versions vary).
+corePackages :: [String]
+corePackages = [
+    "array",
+    "base",
+    "bytestring",
+    "Cabal",
+    "containers",
+    "directory",
+    "extensible-exceptions",
+    "filepath",
+    "ghc-prim",
+    "haskell2010",
+    "haskell98",
+    "hpc",
+    "old-locale",
+    "old-time",
+    "pretty",
+    "process",
+    "random",
+    "template-haskell",
+    "time",
+    "unix"
+  ]
+    
+  
 
 cabal2nix :: GenericPackageDescription -> PkgSHA256 -> Pkg
 cabal2nix cabal sha256 = Pkg pkgname pkgver sha256 url desc lic (map simplify libDeps ++ map simplify exeDeps) (libs++libs')
