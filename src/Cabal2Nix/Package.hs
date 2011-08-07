f81d4c1ff7cefb7f56b65ac44a2d48fd37813661
Andres Loeh <mail@andres-loeh.de>
2011-08-07 09:47:36 +0200

    Layout.

--- src/Cabal2Nix/Package.hs
+++ src/Cabal2Nix/Package.hs
@@ -31,44 +31,44 @@ data Pkg = Pkg PkgName
 
 toNix :: Pkg -> String
 toNix (Pkg name ver sha256 url desc lic deps libs) = render doc
-    where
-      doc = braces (fsep $ punctuate comma $ map text ("cabal" : pkgDeps)) <+>
-              colon $$ text "" $$
-            vcat [
-              text "cabal.mkDerivation" <+> lparen <> text "self" <+>
-                colon <+> lbrace,
-              nest 2 $ vcat [
-                attr "pname"   $ doubleQuotes (text name),
-                attr "version" $ doubleQuotes showVer,
-                attr "sha256"  $ doubleQuotes (text sha256),
-                onlyIf pkgDeps $
-                  sep [
-                    text "propagatedBuildInputs" <+> equals <+> lbrack,
-                    nest 2 $ fsep $ map text pkgDeps,
-                    rbrack <> semi
-                  ],
-                vcat [
-                  text "meta" <+> equals <+> lbrace,
-                  nest 2 $ vcat [
-                    onlyIf url  $ attr "homepage"    $ doubleQuotes (text url),
-                    onlyIf desc $ attr "description" $ doubleQuotes (text desc),
-                    attr "license"     $ text (showLic lic)
-                  ],
-                  rbrace <> semi
-                ]
-              ],
-              rbrace <> rparen,
-              text ""
-            ]
-      attr n v = text n <+> equals <+> v <> semi
-      onlyIf p d = if not (null p) then d else empty
-      showVer = hcat (punctuate (text ".") (map int ver))
-      pkgDeps :: [String]
-      pkgDeps = nub $ sort $ map toNixName $
-                libs ++ [ n | dep <- deps,
-                              Dependency (PackageName n) _ <- condTreeConstraints dep,
-                              n `notElem` corePackages
-                        ]
+  where
+    doc = braces (fsep $ punctuate comma $ map text ("cabal" : pkgDeps)) <+>
+            colon $$ text "" $$
+          vcat [
+            text "cabal.mkDerivation" <+> lparen <> text "self" <+>
+              colon <+> lbrace,
+            nest 2 $ vcat [
+              attr "pname"   $ doubleQuotes (text name),
+              attr "version" $ doubleQuotes showVer,
+              attr "sha256"  $ doubleQuotes (text sha256),
+              onlyIf pkgDeps $
+                sep [
+                  text "propagatedBuildInputs" <+> equals <+> lbrack,
+                  nest 2 $ fsep $ map text pkgDeps,
+                  rbrack <> semi
+                ],
+              vcat [
+                text "meta" <+> equals <+> lbrace,
+                nest 2 $ vcat [
+                  onlyIf url  $ attr "homepage"    $ doubleQuotes (text url),
+                  onlyIf desc $ attr "description" $ doubleQuotes (text desc),
+                  attr "license"     $ text (showLic lic)
+                ],
+                rbrace <> semi
+              ]
+            ],
+            rbrace <> rparen,
+            text ""
+          ]
+    attr n v = text n <+> equals <+> v <> semi
+    onlyIf p d = if not (null p) then d else empty
+    showVer = hcat (punctuate (text ".") (map int ver))
+    pkgDeps :: [String]
+    pkgDeps = nub $ sort $ map toNixName $
+              libs ++ [ n | dep <- deps,
+                            Dependency (PackageName n) _ <- condTreeConstraints dep,
+                            n `notElem` corePackages
+                      ]
 
 -- | List of packages shipped with ghc and therefore at the moment not in
 -- nixpkgs. This should probably be configurable at first. Later, it might
@@ -101,7 +101,10 @@ corePackages = [
   
 
 cabal2nix :: GenericPackageDescription -> PkgSHA256 -> Pkg
-cabal2nix cabal sha256 = Pkg pkgname pkgver sha256 url desc lic (map simplify libDeps ++ map simplify exeDeps) (libs++libs')
+cabal2nix cabal sha256 =
+    Pkg pkgname pkgver sha256 url desc lic
+      (map simplify libDeps ++ map simplify exeDeps)
+      (libs ++ libs')
   where
     pkg = packageDescription cabal
     PackageName pkgname = pkgName (package pkg)
