067e51b049e2fc638673c2266fb5d7a86d84a35a
Peter Simons <simons@cryp.to>
2011-08-07 14:04:00 +0200

    Cabal2Nix/CorePackages.hs: restrict export list

--- src/Cabal2Nix/CorePackages.hs
+++ src/Cabal2Nix/CorePackages.hs
@@ -1,4 +1,4 @@
-module Cabal2Nix.CorePackages where
+module Cabal2Nix.CorePackages ( corePackages ) where
 
 -- | List of packages shipped with ghc and therefore at the moment not in
 -- nixpkgs. This should probably be configurable at first. Later, it might
