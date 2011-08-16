7919758309b106ebc9078b23bce9aedc349da0fc
Peter Simons <simons@cryp.to>
2011-08-16 12:34:17 +0200

    PackageList.hs: don't try to guess the Nix attribute -- nix-env can tell us what it is

--- src/PackageList.hs
+++ src/PackageList.hs
@@ -1,51 +1,51 @@
 module Main ( main ) where
 
-import Text.Regex.Posix
-import System.Process
-import Data.Maybe
 import Data.List
+import Data.Maybe
 import Data.Version
 import Distribution.Text
-import Distribution.Package
-import Cabal2Nix.Name
+import System.Process
+import Text.Regex.Posix
+
+type Pkg    = (String,Version,String) -- (Name, Version, Attribute)
+type Pkgset = [Pkg]
+
+parseHaskellPackageName :: String -> Maybe Pkg
+parseHaskellPackageName name =
+  case name `regsubmatch` "^(haskellPackages[^ \t]+)[ \t]+haskell-(.*)-ghc[0-9.]+-(.*)$" of
+    [attr,name',version] -> case simpleParse version of
+                              Just version' -> Just (name',version',attr)
+                              _             -> Nothing
+    _               -> Nothing
+
+getHaskellPackageList :: IO Pkgset
+getHaskellPackageList = do
+  allPkgs <- fmap lines (readProcess "bash" ["-c", "exec nix-env -qaP \\* 2>/dev/tty"] "")
+  let pkgset = map parseHaskellPackageName allPkgs
+  return (sort (nub (catMaybes pkgset)))
+
+stripProfilingVersions :: Pkgset -> Pkgset
+stripProfilingVersions pkgs = [ p | p@(_,_,attr) <- pkgs , not ("ghc[0-9.]+_profiling" =~ attr) ]
+
+selectLatestVersions :: Pkgset -> Pkgset
+selectLatestVersions = nubBy f2 . sortBy f1
+  where
+    f1 (n1,v1,a1) (n2,v2,a2)
+      | n1 == n2         = compare v2 v1
+      | otherwise        = compare n1 n2
+    f2 (n1,_,_) (n2,_,_) = n1 == n2
+
+formatPackageLine :: Pkg -> String
+formatPackageLine (name,version,attr) = show (name, showVersion version, Just url)
+  where
+    url = "http://hydra.nixos.org/job/nixpkgs/trunk/" ++ attr
 
 regsubmatch :: String -> String -> [String]
 regsubmatch buf patt = let (_,_,_,x) = f in x
   where f :: (String,String,String,[String])
         f = match (makeRegexOpts compExtended execBlank patt) buf
 
-parseHaskellPackageName :: String -> Maybe (String,Version)
-parseHaskellPackageName name =
-  case name `regsubmatch` "^haskell-(.*)-ghc[0-9.]+-(.*)$" of
-    [name',version] -> case simpleParse version of
-                         Just version' -> Just (name',version')
-                         _             -> Nothing
-    _               -> Nothing
-
-stripProfilingTag :: Version -> Version
-stripProfilingTag (Version v tags) = Version v (filter (/="profiling") tags)
-
-getHaskellPackageList :: IO [(String,Version)]
-getHaskellPackageList = do
-  allPkgs <- fmap lines (readProcess "bash" ["-c", "exec nix-env -qa \\* 2>/dev/tty"] "")
-  let hsPkgs  = map parseHaskellPackageName allPkgs
-      pkglist = [ (name, stripProfilingTag version) | Just (name,version) <- hsPkgs ]
-  return (sort (nub pkglist))
-
-selectLatestVersion :: [(String,Version)] -> [(String,Version)]
-selectLatestVersion = nubBy f2 . sortBy f1
-  where
-    f1 (n1,v1) (n2,v2)
-      | n1 == n2     = compare v2 v1
-      | otherwise    = compare n1 n2
-    f2 (n1,v1) (n2,v2) = n1 == n2
-
-formatPackageLine :: (String,Version) -> String
-formatPackageLine (n,v) = show (n, showVersion v, Just url)
-  where
-    url = "http://hydra.nixos.org/job/nixpkgs/trunk/haskellPackages_ghc704_no_profiling."++toNixName n
-
 main :: IO ()
 main = do
-  pkgs <- fmap selectLatestVersion getHaskellPackageList
+  pkgs <- fmap (selectLatestVersions . stripProfilingVersions) getHaskellPackageList
   mapM_ (putStrLn . formatPackageLine) pkgs
