d19c3c2f23b3f6b12f0bf8eb4fdac4e8c21ed765
Peter Simons <simons@cryp.to>
2011-08-23 14:43:48 +0200

    Cabal2Nix/Flags.hs: fixed ghc warnings

--- src/Cabal2Nix/Flags.hs
+++ src/Cabal2Nix/Flags.hs
@@ -2,13 +2,11 @@ module Cabal2Nix.Flags ( configureFlags ) where
 
 import Distribution.Package
 import Distribution.PackageDescription
-import Distribution.PackageDescription.Configuration
 
 configureFlags :: PackageIdentifier -> FlagAssignment
-configureFlags (PackageIdentifier (PackageName name) version)
+configureFlags (PackageIdentifier (PackageName name) _)
  | name == "pandoc"     = [enable "highlighting", enable "threaded"]
  | otherwise            = []
-
 
 enable :: String -> (FlagName,Bool)
 enable name = (FlagName name, True)
