ee21d755702967a512b0300e7c9a441c69004d99
Peter Simons <simons@cryp.to>
2011-09-12 18:27:21 +0200

    Cabal2Nix/PostProcess.hs: refer to libc as 'libc' rather than 'glibc'
    Some packages need libc to include headers like <time.h>, etc. To satisfy
    that dependency, we add a reference to the non-existent attribute 'libc',
    which ought to be overridden as follows in haskell-packages.nix:
    
      foo = callPackage ../development/libraries/haskell/foo {
        libc = pkgs.stdenv.gcc.libc;
      };
    
    On impure platforms, such as Darwin, that reference will be 'null', but on
    Linux it will refer to glibc, as expected.

--- src/Cabal2Nix/PostProcess.hs
+++ src/Cabal2Nix/PostProcess.hs
@@ -8,27 +8,27 @@ import Data.List
 postProcess :: Derivation -> Derivation
 postProcess deriv@(MkDerivation {..})
   | pname == "alex"             = deriv { buildTools = "perl":buildTools }
-  | pname == "cairo"            = deriv { extraLibs = "pkgconfig":"glibc":"cairo":"zlib":extraLibs }
+  | pname == "cairo"            = deriv { extraLibs = "pkgconfig":"libc":"cairo":"zlib":extraLibs }
   | pname == "editline"         = deriv { extraLibs = "libedit":extraLibs }
   | pname == "epic"             = deriv { extraLibs = "gmp":"boehmgc":extraLibs, buildTools = "happy":buildTools }
-  | pname == "glade"            = deriv { extraLibs = "pkgconfig":"glibc":extraLibs, pkgConfDeps = "gtkC":(delete "gtk" pkgConfDeps) }
-  | pname == "glib"             = deriv { extraLibs = "pkgconfig":"glibc":extraLibs }
+  | pname == "glade"            = deriv { extraLibs = "pkgconfig":"libc":extraLibs, pkgConfDeps = "gtkC":(delete "gtk" pkgConfDeps) }
+  | pname == "glib"             = deriv { extraLibs = "pkgconfig":"libc":extraLibs }
   | pname == "GLUT"             = deriv { extraLibs = "glut":"libSM":"libICE":"libXmu":"libXi":"mesa":extraLibs }
-  | pname == "gtk"              = deriv { extraLibs = "pkgconfig":"glibc":extraLibs, buildDepends = delete "gio" buildDepends }
-  | pname == "gtksourceview2"   = deriv { extraLibs = "pkgconfig":"glibc":extraLibs }
+  | pname == "gtk"              = deriv { extraLibs = "pkgconfig":"libc":extraLibs, buildDepends = delete "gio" buildDepends }
+  | pname == "gtksourceview2"   = deriv { extraLibs = "pkgconfig":"libc":extraLibs }
   | pname == "happy"            = deriv { buildTools = "perl":buildTools }
   | pname == "haskell-src"      = deriv { buildTools = "happy":buildTools }
   | pname == "hmatrix"          = deriv { extraLibs = "gsl":"liblapack":"blas":extraLibs }
   | pname == "idris"            = deriv { buildTools = "happy":buildTools }
   | pname == "OpenAL"           = deriv { extraLibs = "openal":extraLibs }
   | pname == "OpenGL"           = deriv { extraLibs = "mesa":"libX11":extraLibs }
-  | pname == "pango"            = deriv { extraLibs = "pkgconfig":"glibc":extraLibs }
+  | pname == "pango"            = deriv { extraLibs = "pkgconfig":"libc":extraLibs }
   | pname == "persistent"       = deriv { extraLibs = "sqlite3":extraLibs }
   | pname == "repa-examples"    = deriv { extraLibs = "llvm":extraLibs }
   | pname == "SDL-image"        = deriv { extraLibs = "SDL_image":extraLibs }
   | pname == "SDL-mixer"        = deriv { extraLibs = "SDL_mixer":extraLibs }
   | pname == "SDL-ttf"          = deriv { extraLibs = "SDL_ttf":extraLibs }
-  | pname == "svgcairo"         = deriv { extraLibs = "glibc":extraLibs }
+  | pname == "svgcairo"         = deriv { extraLibs = "libc":extraLibs }
   | pname == "terminfo"         = deriv { extraLibs = "ncurses":extraLibs }
   | pname == "vacuum"           = deriv { extraLibs = "ghcPaths":extraLibs }
   | pname == "wxcore"           = deriv { extraLibs = "wxGTK":"mesa":"libX11":extraLibs }
