b0b3705eb19d874100512378bd9498647e44289a
Peter Simons <simons@cryp.to>
2011-10-02 22:45:14 +0200

    PostProcess.hs: haddock needs build inputs alex and happy

--- src/Cabal2Nix/PostProcess.hs
+++ src/Cabal2Nix/PostProcess.hs
@@ -16,6 +16,7 @@ postProcess deriv@(MkDerivation {..})
   | pname == "GLUT"             = deriv { extraLibs = "glut":"libSM":"libICE":"libXmu":"libXi":"mesa":extraLibs }
   | pname == "gtk"              = deriv { extraLibs = "pkgconfig":"libc":extraLibs, buildDepends = delete "gio" buildDepends }
   | pname == "gtksourceview2"   = deriv { extraLibs = "pkgconfig":"libc":extraLibs }
+  | pname == "haddock"          = deriv { buildTools = "alex":"happy":buildTools }
   | pname == "happy"            = deriv { buildTools = "perl":buildTools }
   | pname == "haskell-src"      = deriv { buildTools = "happy":buildTools }
   | pname == "hmatrix"          = deriv { extraLibs = "gsl":"liblapack":"blas":extraLibs }
