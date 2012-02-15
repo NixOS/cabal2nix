b1f5d221e30347b97d4bfa685dbc2698c22330aa
Peter Simons <simons@cryp.to>
2012-02-15 11:28:56 +0100

    Cabal2Nix/Flags.hs: pandoc no longer supports the -fhighlighting and -fthreaded flags

--- src/Cabal2Nix/Flags.hs
+++ src/Cabal2Nix/Flags.hs
@@ -6,7 +6,6 @@ import Distribution.PackageDescription
 configureCabalFlags :: PackageIdentifier -> FlagAssignment
 configureCabalFlags (PackageIdentifier (PackageName name) _)
  | name == "hmatrix"            = [enable "vector"]
- | name == "pandoc"             = [enable "highlighting", enable "threaded"]
  | name == "xmobar"             = [enable "with_xft"]
  | otherwise                    = []
 
