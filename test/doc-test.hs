ae752f64f5cd84446687d71b62c29801766de65a
Peter Simons <simons@cryp.to>
2013-02-24 22:39:04 +0100

    Added a doctest suite.

---
+++ test/doc-test.hs
@@ -0,0 +1,33 @@
+{-
+   Module      :  Main
+   Copyright   :  (c) 2013 Peter Simons
+   License     :  BSD3
+
+   Maintainer  :  simons@cryp.to
+   Stability   :  provisional
+   Portability :  portable
+
+   Cabal2nix doctest suite.
+-}
+
+module Main ( main ) where
+
+import Test.DocTest
+
+main :: IO ()
+main = do
+  let libs = [ "src/Cabal2Nix/License.hs"
+             , "src/Cabal2Nix/CorePackages.hs"
+             , "src/Cabal2Nix/Flags.hs"
+             , "src/Cabal2Nix/Generate.hs"
+             , "src/Cabal2Nix/Hackage.hs"
+             , "src/Cabal2Nix/Normalize.hs"
+             , "src/Cabal2Nix/Name.hs"
+             , "src/Cabal2Nix/PostProcess.hs"
+             , "src/Distribution/NixOS/Derivation/License.hs"
+             , "src/Distribution/NixOS/Derivation/Cabal.hs"
+             , "src/Distribution/NixOS/Derivation/Meta.hs"
+             , "src/Distribution/NixOS/PrettyPrinting.hs"
+             ]
+  doctest $ "src/Cabal2Nix.hs" : libs
+  doctest $ "src/Hackage4Nix.hs" : libs
