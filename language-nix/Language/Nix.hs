09362ea8e644101f6435203e0899d8a01638753e
Peter Simons <simons@cryp.to>
2013-03-01 14:54:22 +0100

    Re-factor code into a proper library.

---
+++ language-nix/Language/Nix.hs
@@ -0,0 +1,330 @@
+{- |
+   Module      :  Language.Nix
+   Copyright   :  (c) 2013 Peter Simons
+   License     :  BSD3
+   Maintainer  :  simons@cryp.to
+ -}
+
+module Language.Nix
+  ( -- * Running the Parser
+    parseNixFile, parse, parse', ParseError,
+
+    -- * Nix Language AST
+    Expr(..), ScopedIdent(..), Attr(..), genIdentifier,
+
+    -- * Nix Language Parsers
+    expr, listExpr, term, operatorTable, listOperatorTable, identifier, literal,
+    nixString, literalURI, attrSet, scopedIdentifier, attribute, list, letExpr,
+    letAssignment, attrSetPattern,
+
+    -- * Parsec Language Specification
+    TokenParser, LanguageDef, NixParser, NixOperator, nixLanguage, nixLexer,
+    symbol, reserved, reservedOp, lexeme, parens, braces, brackets, natural,
+    assign, semi, dot, commaSep1, whitespace,
+  )
+  where
+
+import Data.Functor.Identity
+import Control.Applicative ( (<$>), (<*>), (<$), (<*), (*>) )
+import Text.Parsec hiding ( parse )
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Language as Parsec
+import qualified Text.Parsec.Token as Parsec
+import Text.Parsec.Expr
+import Text.PrettyPrint.Leijen ( Pretty(..) )
+import qualified Text.PrettyPrint.Leijen as Pretty
+import Test.QuickCheck
+
+----- Nix Language Definition for Parsec --------------------------------------
+
+type TokenParser = Parsec.GenTokenParser String () Identity
+type LanguageDef = Parsec.GenLanguageDef String () Identity
+type NixParser a = ParsecT String () Identity a
+type NixOperator = Operator String () Identity Expr
+
+nixLanguage :: LanguageDef
+nixLanguage = Parsec.emptyDef
+  { Parsec.commentStart    = "/*"
+  , Parsec.commentEnd      = "*/"
+  , Parsec.commentLine     = "#"
+  , Parsec.nestedComments  = False
+  , Parsec.identStart      = letter <|> oneOf "_"
+  , Parsec.identLetter     = alphaNum <|> oneOf "-_"
+  , Parsec.opStart         = Parsec.opLetter nixLanguage
+  , Parsec.opLetter        = oneOf ".!{}[]+=?&|/:"
+  , Parsec.reservedOpNames = [".","!","+","++","&&","||","?","=","//","==","!=",":"]
+  , Parsec.reservedNames   = ["rec","let","in","import","with","inherit","assert","or","if","then","else"]
+  , Parsec.caseSensitive   = True
+  }
+
+nixLexer :: TokenParser
+nixLexer = Parsec.makeTokenParser nixLanguage
+
+symbol :: String -> NixParser String
+symbol = Parsec.symbol nixLexer
+
+reserved :: String -> NixParser ()
+reserved = Parsec.reserved nixLexer
+
+reservedOp :: String -> NixParser ()
+reservedOp = Parsec.reservedOp nixLexer
+
+lexeme :: NixParser a -> NixParser a
+lexeme = Parsec.lexeme nixLexer
+
+parens :: NixParser a -> NixParser a
+parens = Parsec.parens nixLexer
+
+braces :: NixParser a -> NixParser a
+braces = Parsec.braces nixLexer
+
+brackets :: NixParser a -> NixParser a
+brackets = Parsec.brackets nixLexer
+
+natural :: NixParser String
+natural = show <$> Parsec.natural nixLexer
+
+assign :: NixParser String
+assign = symbol "="
+
+semi :: NixParser String
+semi = Parsec.semi nixLexer
+
+dot :: NixParser String
+dot = Parsec.dot nixLexer
+
+commaSep1 :: NixParser a -> NixParser [a]
+commaSep1 = Parsec.commaSep1 nixLexer
+
+whitespace :: NixParser ()
+whitespace = Parsec.whiteSpace nixLexer
+
+----- Nix Expressions ---------------------------------------------------------
+
+newtype ScopedIdent = SIdent [String]
+  deriving (Read, Show, Eq)
+
+data Attr = Assign ScopedIdent Expr
+          | Inherit ScopedIdent [String]
+  deriving (Read, Show, Eq)
+
+genIdentifier :: Gen String
+genIdentifier = ((:) <$> elements firstChar <*> listOf (elements identChar)) `suchThat` (`notElem` Parsec.reservedNames nixLanguage)
+  where firstChar = ['a'..'z'] ++ ['A'..'Z'] ++ "_"
+        identChar = firstChar ++ ['0'..'9'] ++ "-"
+
+instance Arbitrary ScopedIdent where
+  arbitrary = SIdent <$> listOf1 genIdentifier
+
+instance Pretty ScopedIdent where
+  pretty (SIdent xs) = Pretty.hcat $ Pretty.punctuate Pretty.dot (map Pretty.text xs)
+
+data Expr = Lit String
+          | Ident String
+          | AttrSet Bool [Attr]
+          | AttrSetP (Maybe String) [(String, Maybe Expr)]
+          | List [Expr]
+          | Deref Expr Expr
+          | HasAttr Expr Expr
+          | DefAttr Expr Expr
+          | Concat Expr Expr
+          | Append Expr Expr
+          | Not Expr
+          | Union Expr Expr
+          | Equal Expr Expr
+          | Inequal Expr Expr
+          | And Expr Expr
+          | Or Expr Expr
+          | Implies Expr Expr
+          | Fun Expr Expr
+          | Let [(String,Expr)] Expr
+          | Apply Expr Expr
+          | Import Expr
+          | With Expr
+          | Assert Expr
+          | IfThenElse Expr Expr Expr
+  deriving (Read, Show, Eq)
+
+expr :: NixParser Expr
+expr = whitespace >> buildExpressionParser operatorTable term
+
+listExpr :: NixParser Expr
+listExpr = buildExpressionParser listOperatorTable term
+
+term :: NixParser Expr
+term = choice [ parens expr
+              , list
+              , try attrSetPattern
+              , attrSet
+              , letExpr
+              , reserved "import" >> Import <$> expr
+              , reserved "with" >> With <$> expr <* semi
+              , reserved "assert" >> Assert <$> expr <* semi
+              , IfThenElse <$> (reserved "if" *> expr) <*> (reserved "then" *> expr) <*> (reserved "else" *> expr)
+              , try literal
+              , identifier
+              ]
+
+operatorTable :: [[NixOperator]]
+operatorTable = x1 : x2 : x3 : [ Infix (Apply <$ whitespace) AssocRight ] : xs
+  where (x1:x2:x3:xs) = listOperatorTable
+
+listOperatorTable :: [[NixOperator]]
+listOperatorTable = [ [ binary ":" Fun AssocRight ]
+                    , [ binary "." Deref AssocLeft ]
+                    , [ binary "or" DefAttr AssocNone ]
+                {-  , [ Infix (Apply <$ whitespace) AssocRight ] -}
+                    , [ binary "?" HasAttr AssocNone ]
+                    , [ binary "++" Concat AssocRight ]
+                    , [ binary "+" Append AssocLeft ]
+                    , [ prefix "!" Not ]
+                    , [ binary "//" Union AssocRight ]
+                    , [ binary "==" Equal AssocNone ]
+                    , [ binary "!=" Inequal AssocNone ]
+                    , [ binary "&&" And AssocLeft ]
+                    , [ binary "||" Or AssocLeft ]
+                    , [ binary "->" Implies AssocNone ]
+                    ]
+  where
+    binary :: String -> (Expr -> Expr -> Expr) -> Assoc -> NixOperator
+    binary op fun = Infix (fun <$ reservedOp op)
+
+    prefix :: String -> (Expr -> Expr) -> NixOperator
+    prefix op fun = Prefix (fun <$ reservedOp op)
+
+identifier :: NixParser Expr
+identifier = Ident <$> Parsec.identifier nixLexer
+
+literal :: NixParser Expr
+literal = Lit <$> (Parsec.stringLiteral nixLexer <|> nixString <|> natural <|> literalURI)
+
+nixString :: NixParser String
+nixString = lexeme $ between (string "''") (string "''") (many (noneOf "'" <|> try (char '\'' <* notFollowedBy (char '\''))))
+
+literalURI :: NixParser String
+literalURI = lexeme $ try absoluteURI <|> relativeURI
+
+absoluteURI :: NixParser String
+absoluteURI = (++) <$> scheme <*> ((:) <$> char ':' <*> (hierPart <|> opaquePart))
+
+relativeURI :: NixParser String
+relativeURI = (++) <$> (absPath <|> relPath) <*> option "" (char '?' >> query)
+
+absPath :: NixParser String
+absPath = (:) <$> char '/' <*> pathSegments
+
+authority :: NixParser String
+authority = server <|> regName
+
+domainlabel :: NixParser String
+domainlabel = (:) <$> alphaNum <*> option "" ((++) <$> many (char '-') <*> domainlabel)
+
+escapedChars :: NixParser Char
+escapedChars = char '%' >> hexDigit >> hexDigit
+
+hierPart :: NixParser String
+hierPart = (++) <$> (try netPath <|> absPath) <*> option "" (char '?' >> query)
+
+host :: NixParser String
+host = hostname <|> ipv4address
+
+hostname :: NixParser String
+hostname = many (domainlabel >> char '.') >> toplabel >> option "" (string ".")
+
+hostport :: NixParser String
+hostport = (++) <$> host <*> option "" ((:) <$> char ':' <*> port)
+
+ipv4address :: NixParser String
+ipv4address = many1 digit >> char '.' >> many1 digit >> char '.' >> many1 digit >> char '.' >> many1 digit
+
+markChars :: NixParser Char
+markChars = oneOf "-_.!~*'" -- Note that "()" have been removed here!
+
+netPath :: NixParser String
+netPath = (++) <$> ((++) <$> string "//" <*> authority) <*> option "" absPath
+
+opaquePart :: NixParser String
+opaquePart = uricNoSlash >> many uric
+
+pathSegments :: NixParser String
+pathSegments = (++) <$> segment <*> (concat <$> many ((:) <$> char '/' <*> segment))
+
+pchar :: NixParser Char
+pchar = unreservedChars <|> escapedChars <|> oneOf ":@&=+$,"
+
+port :: NixParser String
+port = many1 digit
+
+query :: NixParser String
+query = many uric
+
+regName :: NixParser String
+regName = many1 (unreservedChars <|> escapedChars <|> oneOf "$,:@&=+") -- Note that ';' has been removed here!
+
+relPath :: NixParser String
+relPath = (++) <$> relSegment <*> absPath
+
+relSegment :: NixParser String
+relSegment = many1 (unreservedChars <|> escapedChars <|> oneOf "@&=+$,") -- Note that ';' has been removed here!
+
+reservedChars :: NixParser Char
+reservedChars = oneOf "/?:@&=+$," -- Note that ';' has been removed here!
+
+scheme :: NixParser String
+scheme = (:) <$> letter <*> many (alphaNum <|> oneOf "+-.")
+
+segment :: NixParser String
+segment = {- (++) <$> -} many pchar {- <*> (concat <$> many ((:) <$> char ';' <*> param)) -}
+
+server :: NixParser String
+server = option "" (option "" ((++) <$> userinfo <*> string "@") >> hostport)
+
+toplabel :: NixParser Char
+toplabel = letter <|> (letter >> many (alphaNum <|> char '-') >> alphaNum)
+
+unreservedChars :: NixParser Char
+unreservedChars = alphaNum <|> markChars
+
+uric :: NixParser Char
+uric = reservedChars <|> unreservedChars <|> escapedChars
+
+uricNoSlash :: NixParser Char
+uricNoSlash = unreservedChars <|> escapedChars <|> oneOf ";?:@&=+$,"
+
+userinfo :: NixParser String
+userinfo = many (unreservedChars <|> escapedChars <|> oneOf ";:&=+$,")
+
+attrSet :: NixParser Expr
+attrSet = AttrSet <$> option False (True <$ reserved "rec") <*> braces (attribute `endBy` semi)
+
+scopedIdentifier :: NixParser ScopedIdent
+scopedIdentifier = SIdent <$> sepBy1 (Parsec.identifier nixLexer) dot
+
+attribute :: NixParser Attr
+attribute =  (Assign <$> (SIdent . return <$> Parsec.stringLiteral nixLexer <|> scopedIdentifier) <* assign <*> expr)
+         <|> (Inherit <$> (symbol "inherit" *> option (SIdent []) (parens scopedIdentifier)) <*> many1 (Parsec.identifier nixLexer))
+
+list :: NixParser Expr
+list = List <$> brackets (many listExpr)
+
+attrSetPattern :: NixParser Expr
+attrSetPattern = AttrSetP <$> optionMaybe atPattern <*> setPattern
+  where
+    atPattern  = Parsec.identifier nixLexer <* reserved "@"
+    setPattern = braces $ commaSep1 $ (,) <$> Parsec.identifier nixLexer <*> optionMaybe (reservedOp "?" >> expr) <|> ellipsis
+    ellipsis   = ("...",Nothing) <$ reserved "..."
+
+letExpr :: NixParser Expr
+letExpr = Let <$> (reserved "let" *> many1 letAssignment) <*> (reserved "in" *> expr)
+
+letAssignment :: NixParser (String, Expr)
+letAssignment = (,) <$> Parsec.identifier nixLexer <* assign <*> expr <* semi
+
+parseNixFile :: FilePath -> IO (Either ParseError Expr)
+parseNixFile path = Parsec.parse expr path <$> readFile path
+
+parse' :: NixParser a -> String -> FilePath -> Either ParseError a
+parse' = Parsec.parse
+
+parse :: NixParser a -> String -> Either ParseError a
+parse p input = parse' (p <* eof) (show input) input
---
+++ language-nix/self-test.hs
@@ -0,0 +1,163 @@
+-- self-test.hs
+
+module Main ( main ) where
+
+import Language.Nix
+import Text.PrettyPrint.Leijen ( pretty )
+import qualified Text.Parsec.Token as Parsec ( reservedNames )
+import Test.DocTest
+import Test.QuickCheck
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.HUnit.Base ( assertFailure, assertEqual )
+
+gives :: (Eq a, Show a) => Either ParseError a -> a -> Expectation
+gives x y = either (assertFailure . msg) (assertEqual "" y) x
+  where msg z = "expected: " ++ show y ++ "\nbut got parser error: " ++ show z
+
+parseFail :: Show a => NixParser a -> String -> Expectation
+parseFail p input = parse p input `shouldSatisfy` either (const True) (const False)
+
+main :: IO ()
+main = do
+  doctest [ "Language/Nix.hs" ]
+  hspec $ do
+    describe "identifier" $ do
+      it "parses hand-picked sample inputs" $ do
+        parse identifier "abc" `gives` Ident "abc"
+        parse identifier "abc  " `gives` Ident "abc"
+        parse identifier "__a-b-c-__  " `gives` Ident "__a-b-c-__"
+      prop "parses all randomly generated samples" $
+        forAll genIdentifier $ \input -> either (const False) (Ident input ==) (parse identifier input)
+      it "does not swallow leading whitespace" $
+        parseFail identifier " abc"
+      it "does not parse a de-referencing expression" $
+        parseFail identifier "abc.def"
+      it "does not accept reserved words" $
+        mapM_ (parseFail identifier) (Parsec.reservedNames nixLanguage)
+      it "accepts identifiers that are a prefix of a reserved word" $
+        parse identifier "lett" `gives` Ident "lett"
+
+    describe "scopedIdentifier" $ do
+      it "parses hand-picked sample inputs" $ do
+        parse scopedIdentifier "abc" `gives` SIdent ["abc"]
+        parse scopedIdentifier "abc  " `gives` SIdent ["abc"]
+        parse scopedIdentifier "abc.def" `gives` SIdent ["abc","def"]
+      prop "parses all randomly generated samples" $
+        \sident -> either (const False) (sident ==) (parse scopedIdentifier (show (pretty sident)))
+      it "does not swallow leading whitespace" $
+        parseFail scopedIdentifier " abc"
+      it "does not accept reserved words" $
+        mapM_ (parseFail scopedIdentifier) (Parsec.reservedNames nixLanguage)
+      it "accepts identifiers that are a prefix of a reserved word" $
+        parse scopedIdentifier "lett" `gives` SIdent ["lett"]
+
+    describe "literal" $ do
+      prop "parses all randomly generated literal strings" $
+        \str -> either (const False) (Lit str ==) (parse literal (show str))
+      prop "parses all randomly generated integers" $
+        \n -> either (const False) (Lit (show (abs (n::Int))) ==) (parse literal (show (abs n)))
+      it "parses paths" $
+        parse literal "claus/ist/der/beste" `gives` Lit "claus/ist/der/beste"
+      it "parses URIs" $
+        parse literal "http://example.org" `gives` Lit "http://example.org"
+
+    describe "attrSet" $ do
+      it "parses an empty attribute set" $ do
+        parse attrSet "{}" `gives` AttrSet False []
+        parse attrSet "rec {}" `gives` AttrSet True []
+      it "parses hand-picked sample inputs" $ do
+        parse attrSet "{ a = b; }" `gives` AttrSet False [Assign (SIdent ["a"]) (Ident "b")]
+        parse attrSet "{ a = b.c; }" `gives` AttrSet False [Assign (SIdent ["a"]) (Deref (Ident "b") (Ident "c"))]
+        parse attrSet "{ a = \"b\"; }" `gives` AttrSet False [Assign (SIdent ["a"]) (Lit "b")]
+      it "parses attribute sets as values of attribute sets" $
+        parse attrSet "{ a = { b = c; }; }" `gives` AttrSet False [Assign (SIdent ["a"]) (AttrSet False [Assign (SIdent ["b"]) (Ident "c")])]
+      it "expects assignments to terminated by a semicolon" $
+        parseFail attrSet "{ a = b }"
+      it "ignores comments" $
+        parse attrSet "{ /* foo */ a = /* bar */ b; # foobar\n }" `gives` AttrSet False [Assign (SIdent ["a"]) (Ident "b")]
+      it "parses recursive attribute sets" $
+        parse attrSet "rec { a = b; b = a; }" `gives` AttrSet True [Assign (SIdent ["a"]) (Ident "b"), Assign (SIdent ["b"]) (Ident "a")]
+      it "parses inherit statements" $ do
+        parse attrSet "{ inherit a; }" `gives` AttrSet False [Inherit (SIdent []) ["a"]]
+        parse attrSet "{ inherit a; inherit b; }" `gives` AttrSet False [Inherit (SIdent []) ["a"],Inherit (SIdent []) ["b"]]
+        parse attrSet "{ inherit a b; }" `gives` AttrSet False [Inherit (SIdent []) ["a","b"]]
+        parse attrSet "{ inherit (a) b c d; }" `gives` AttrSet False [Inherit (SIdent ["a"]) ["b","c","d"]];
+
+    describe "list" $ do
+      it "parses an empty list" $
+        parse list "[]" `gives` List []
+      it "parses hand-picked sample inputs" $ do
+        parse list "[ a b c ]" `gives` List [Ident "a",Ident "b",Ident "c"]
+        parse list "[ \"b\" { a = [\"c\"]; } d ]" `gives` List [Lit "b",AttrSet False [Assign (SIdent ["a"]) (List [Lit "c"])],Ident "d"]
+        parse list "[ (a b) c ]" `gives` List [Apply (Ident "a") (Ident "b"),Ident "c"]
+        parse list "[ 12 8 a 0 ]" `gives` List [Lit "12", Lit "8", Ident "a", Lit "0"]
+
+    describe "reserved" $ do
+      it "parses a specific reserved name" $ do
+         parse (reserved "let") "let" `gives` ()
+         parse (reserved "in") "in" `gives` ()
+         parse (reserved "rec") "rec" `gives` ()
+         parseFail (reserved "rec") "let"
+      it "recognizes if the keyword is actually just a prefix of the input string" $
+         parseFail (reserved "in") "input"
+
+    describe "expr" $ do
+      it "parses an empty attribute set" $ do
+        parse expr "{}" `gives` AttrSet False []
+        parse expr "rec {}" `gives` AttrSet True []
+      it "parses an empty list" $
+        parse expr "[]" `gives` List []
+      it "parses a de-referencing expression" $ do
+        parse expr "abc.def" `gives` Deref (Ident "abc") (Ident "def")
+        parse expr "a.b.c" `gives` Deref (Deref (Ident "a") (Ident "b")) (Ident "c")
+      it "parses recursive attribute sets" $
+        parse expr "rec { id = x: x; }" `gives` AttrSet True [Assign (SIdent ["id"]) (Fun (Ident "x") (Ident "x"))]
+      it "parses boolean expressions" $ do
+        parse expr "true" `gives` Ident "true"
+        parse expr "false" `gives` Ident "false"
+        parse expr "system == \"linux\"" `gives` Equal (Ident "system") (Lit "linux")
+        parse expr "system != \"linux\"" `gives` Inequal (Ident "system") (Lit "linux")
+        parse expr "true && true" `gives` And (Ident "true") (Ident "true")
+        parse expr "false || false" `gives` Or (Ident "false") (Ident "false")
+        parse expr "isLinux || isDarwin" `gives` Or (Ident "isLinux") (Ident "isDarwin")
+        parse expr "(isLinux || isDarwin)" `gives` Or (Ident "isLinux") (Ident "isDarwin")
+        parse expr "(isLinux) || (isDarwin)" `gives` Or (Ident "isLinux") (Ident "isDarwin")
+        parse expr "!(!isLinux) || (isDarwin)" `gives` Or (Not (Not (Ident "isLinux"))) (Ident "isDarwin")
+        parse expr "!a && b || c" `gives` Or (And (Not (Ident "a")) (Ident "b")) (Ident "c")
+        parse expr "a && b || c" `gives` Or (And (Ident "a") (Ident "b")) (Ident "c")
+        parse expr "a || b && c" `gives` Or (Ident "a") (And (Ident "b") (Ident "c"))
+        parse expr "(a -> b) -> c" `gives` Implies (Implies (Ident "a") (Ident "b")) (Ident "c")
+      it "parses simple lambda expressions" $ do
+        parse expr "x: {}" `gives` Fun (Ident "x") (AttrSet False [])
+        parse expr "x: y: rec{}" `gives` Fun (Ident "x") (Fun (Ident "y") (AttrSet True []))
+      it "parses attribute set patterns" $ do
+        parse expr "{}: {}" `gives` Fun (AttrSet False []) (AttrSet False [])
+        parse expr "{a ? b, c}: {}" `gives` Fun (AttrSetP Nothing [("a",Just (Ident "b")),("c",Nothing)]) (AttrSet False [])
+        parse expr "{ id = x: x; }" `gives` AttrSet False [Assign (SIdent ["id"]) (Fun (Ident "x") (Ident "x"))]
+        parse expr "{ a?null, b }: rec {}" `gives` Fun (AttrSetP Nothing [("a",Just (Ident "null")),("b",Nothing)]) (AttrSet True [])
+        parse expr "{ a?c.d }: {}" `gives` Fun (AttrSetP Nothing [("a",Just (Deref (Ident "c") (Ident "d")))]) (AttrSet False [])
+        parse expr "{ a?c.d, ... }: {}" `gives` Fun (AttrSetP Nothing [("a",Just (Deref (Ident "c") (Ident "d"))),("...",Nothing)]) (AttrSet False [])
+        parse expr "e@{ a?c.d, ... }: {}" `gives` Fun (AttrSetP (Just "e") [("a",Just (Deref (Ident "c") (Ident "d"))),("...",Nothing)]) (AttrSet False [])
+      it "ignores leading/trailing whitespace" $ do
+        parse expr "   {}" `gives` AttrSet False []
+        parse expr "{}   " `gives` AttrSet False []
+        parse expr " { } " `gives` AttrSet False []
+      it "ignores comments" $
+        parse expr "# foo\n/* bar \n */ { /* bla */ }" `gives` AttrSet False []
+      it "parses function application" $ do
+        parse expr "a b c" `gives` Apply (Ident "a") (Apply (Ident "b") (Ident "c"))
+        parse expr "a.b c" `gives` Apply (Deref (Ident "a") (Ident "b")) (Ident "c")
+        parse expr "a{b=c.d;}" `gives` Apply (Ident "a") (AttrSet False [Assign (SIdent ["b"]) (Deref (Ident "c") (Ident "d"))])
+      it "parses import statements" $ do
+        parse expr "(import ../some/function.nix) c" `gives` Apply (Import (Lit "../some/function.nix")) (Ident "c")
+        parse expr "let x = import ../some/function.nix; in x" `gives` Let [("x",Import (Lit "../some/function.nix"))] (Ident "x")
+      it "parses if-then-else statements" $
+        parse expr "if a b then c { inherit d; } else e" `gives` IfThenElse (Apply (Ident "a") (Ident "b")) (Apply (Ident "c") (AttrSet False [Inherit (SIdent []) ["d"]])) (Ident "e")
+      it "parses with statements" $
+        parse expr "with a; a" `gives` Apply (With (Ident "a")) (Ident "a")
+
+allPackages, top, nixos :: IO (Either ParseError Expr)
+allPackages = parseNixFile "/home/simons/.nix-defexpr/pkgs/top-level/all-packages.nix"
+top = parseNixFile "/home/simons/.nix-defexpr/default.nix"
+nixos = parseNixFile "/etc/nixos/configuration.nix"
