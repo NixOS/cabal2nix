3202c3e57f72bc080b8389f5d0558ee5cc6c4b82
Peter Simons <simons@cryp.to>
2013-08-12 20:36:04 +0200

    Flags.hs: disable iwlib support in xmonad; the wireless library doesn't work with our modern Linux kernels anyway

--- src/Cabal2Nix/Flags.hs
+++ src/Cabal2Nix/Flags.hs
@@ -13,7 +13,7 @@ configureCabalFlags (PackageIdentifier (PackageName name) _)
                                   , enable "Production", enable "TDFA"]
  | name == "haskeline"          = [enable "terminfo"]
  | name == "reactive-banana-wx" = [disable "buildExamples"]
- | name == "xmobar"             = [enable "with_xft", enable "with_iwlib"]
+ | name == "xmobar"             = [enable "with_xft"]
  | name == "xmonad-extras"      = [disable "with_hlist", enable "with_split", enable "with_parsec"]
  | otherwise                    = []
 
