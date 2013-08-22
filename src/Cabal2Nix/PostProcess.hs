d8ddf412ac2c91e4dbb8b01562b406f7bade1ee9
Peter Simons <simons@cryp.to>
2013-08-22 11:50:35 +0200

    PostProcess.hs: ensure that 'pcap' finds the libpcap extra library

--- src/Cabal2Nix/PostProcess.hs
+++ src/Cabal2Nix/PostProcess.hs
@@ -48,6 +48,7 @@ postProcess deriv@(MkDerivation {..})
   | pname == "OpenAL"           = deriv { extraLibs = "openal":extraLibs }
   | pname == "OpenGL"           = deriv { extraLibs = "mesa":"libX11":extraLibs }
   | pname == "pango"            = deriv { extraLibs = "pkgconfig":"libc":extraLibs }
+  | pname == "pcap"             = deriv { extraLibs = "libpcap":extraLibs }
   | pname == "persistent"       = deriv { extraLibs = "sqlite3":extraLibs }
   | pname == "repa-algorithms"  = deriv { extraLibs = "llvm":extraLibs }
   | pname == "repa-examples"    = deriv { extraLibs = "llvm":extraLibs }
