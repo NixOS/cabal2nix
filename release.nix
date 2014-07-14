243b9a928090b16a6bb524bb0fd54fc668f0a22f
Peter Simons <simons@cryp.to>
2014-07-14 10:45:12 +0200

    release.nix: update builds to ghc 7.8.3

--- release.nix
+++ release.nix
@@ -2,7 +2,7 @@
 
 { cabal2nixSrc ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
 , supportedPlatforms ? [ "x86_64-linux" ]
-, supportedCompilers ? ["ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763" "ghc782" "ghcHEAD"]
+, supportedCompilers ? ["ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763" "ghc783" "ghcHEAD"]
 }:
 
 let
