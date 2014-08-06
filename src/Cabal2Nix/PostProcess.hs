107126b25565e7690d9b8ce4c11130708351d7a1
Peter Simons <simons@cryp.to>
2014-08-06 12:16:34 +0200

    Update post-process hook for ghc-mod.

--- src/Cabal2Nix/PostProcess.hs
+++ src/Cabal2Nix/PostProcess.hs
@@ -145,6 +145,7 @@ ghcModPostInstall = unlines
   [ "configureFlags = \"--datasubdir=${self.pname}-${self.version}\";"
   , "postInstall = ''"
   , "  cd $out/share/$pname-$version"
+  , "  sed -i -e 's/\"-b\" \"\\\\n\" \"-l\"/\"-l\" \"-b\" \"\\\\\"\\\\\\\\n\\\\\"\"/' ghc-process.el"
   , "  make"
   , "  rm Makefile"
   , "  cd .."
