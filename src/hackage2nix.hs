c9a64e69822ec65c5686e2514594410f763f962f
Peter Simons <simons@cryp.to>
2014-10-19 20:31:09 +0200

    hackage2nix.hs: update core packages for ghc 7.8.3

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -192,18 +192,19 @@ displayList = intercalate ", " . Prelude.map display
 
 corePackages :: [PackageIdentifier]
 corePackages = Prelude.map (fromJust . simpleParse)
-               [ "Cabal-1.18.1.3"
-               , "array-0.5.0.0"
-               , "base-4.7.0.0"
-               , "bin-package-db-0.0.0.0"
+               [ "array-0.5.0.0"
+               , "base-4.7.0.1"
                , "binary-0.7.1.0"
+               , "bin-package-db-0.0.0.0"
                , "bytestring-0.10.4.0"
+               , "Cabal-1.18.1.3"
                , "containers-0.5.5.1"
                , "deepseq-1.3.0.2"
                , "directory-1.2.1.0"
                , "filepath-1.3.0.2"
-               , "ghc-7.8.2"
+               , "ghc-7.8.3"
                , "ghc-prim-0.3.1.0"
+               , "haskeline-0.7.1.2"
                , "haskell2010-1.1.2.0"
                , "haskell98-2.0.0.3"
                , "hoopl-3.10.0.1"
@@ -215,9 +216,11 @@ corePackages = Prelude.map (fromJust . simpleParse)
                , "process-1.2.0.0"
                , "rts-1.0"
                , "template-haskell-2.9.0.0"
+               , "terminfo-0.4.0.0"
                , "time-1.4.2"
                , "transformers-0.3.0.0"
                , "unix-2.7.0.1"
+               , "xhtml-3000.2.1"
                ]
 
 instance NFData State where
