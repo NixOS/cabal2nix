c462d078bff1be2d27899aecd6b6fbdc73f18042
Peter Simons <simons@cryp.to>
2014-12-03 14:20:10 +0100

    doc-test.hs: add missing Cabal2Nix.Version module to fix the test suite

--- test/doc-test.hs
+++ test/doc-test.hs
@@ -16,16 +16,18 @@ import Test.DocTest
 
 main :: IO ()
 main = do
-  let libs = [ "src/Cabal2Nix/License.hs"
+  let libs = [ "dist/build/autogen/Paths_cabal2nix.hs"
+             , "src/Cabal2Nix/License.hs"
              , "src/Cabal2Nix/CorePackages.hs"
              , "src/Cabal2Nix/Flags.hs"
              , "src/Cabal2Nix/Generate.hs"
-             , "src/Cabal2Nix/Package.hs"
-             , "src/Cabal2Nix/Normalize.hs"
              , "src/Cabal2Nix/Name.hs"
+             , "src/Cabal2Nix/Normalize.hs"
+             , "src/Cabal2Nix/Package.hs"
              , "src/Cabal2Nix/PostProcess.hs"
-             , "src/Distribution/NixOS/Derivation/License.hs"
+             , "src/Cabal2Nix/Version.hs"
              , "src/Distribution/NixOS/Derivation/Cabal.hs"
+             , "src/Distribution/NixOS/Derivation/License.hs"
              , "src/Distribution/NixOS/Derivation/Meta.hs"
              , "src/Distribution/NixOS/Fetch.hs"
              , "src/Distribution/NixOS/PrettyPrinting.hs"
