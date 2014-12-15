0243ddf5dfa3f14b2de7e1dc5246d914a69dfa10
Peter Simons <simons@cryp.to>
2014-12-15 21:43:06 +0100

    hackage2nix: retrieved editedCabalFile information for Hackage packages
    TODO: Generate that hash from the locally available Cabal file rather
    than accessing the network.

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -9,6 +9,7 @@ import Control.Monad
 import Control.Monad.Par.Combinator
 import Control.Monad.Par.IO
 import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
 import Data.Map ( Map )
 import qualified Data.Map as Map
 import Data.Monoid
@@ -16,11 +17,22 @@ import Data.Set ( Set )
 import qualified Data.Set as Set
 import Distribution.Hackage.DB ( Hackage, readHackage, GenericPackageDescription )
 import Distribution.NixOS.Derivation.Cabal
+import Distribution.NixOS.Fetch
 import Distribution.NixOS.PrettyPrinting hiding ( (<>) )
 import Distribution.Text
+import Distribution.PackageDescription hiding ( extraLibs )
 
 main :: IO ()
 main = readHackage >>= runParIO . generatePackageSet
+
+getEditedCabalFileHash :: String -> Version -> IO String
+getEditedCabalFileHash name version =
+  fmap (maybe failure (derivHash . fst)) (runMaybeT $ fetchWith (False,"url",[]) srcspec)
+  where
+    url = "mirror://hackage/" ++ pkgid ++ "/" ++ name ++ ".cabal"
+    srcspec = Source url "" Nothing
+    pkgid = name ++ "-" ++ display version
+    failure = fail $ "cannot determine edited cabal file hash for " ++ pkgid
 
 nixAttr :: String -> Version -> String
 nixAttr name _ = name -- ++ "_" ++ [ if c == '.' then '_' else c | c <- display ver ]
@@ -36,8 +48,8 @@ generatePackageSet db = do
   liftIO $ putStrLn ""
   liftIO $ putStrLn "self: {"
   liftIO $ putStrLn ""
-  forM_ pkgs $ \(name, version, nixExpr) -> do
-    let conflicts = Set.toAscList $ selectHackageNames $ Set.fromList (extraLibs nixExpr ++ pkgConfDeps nixExpr)
+  forM_ pkgs $ \(name, version, drv) -> do
+    let conflicts = Set.toAscList $ selectHackageNames $ Set.fromList (extraLibs drv ++ pkgConfDeps drv)
         overrides | null conflicts = empty
                   | otherwise      = text " inherit (pkgs) " <> hsep (map text conflicts) <> text "; "
     -- when (name /= toNixName' name) $
@@ -45,7 +57,7 @@ generatePackageSet db = do
     -- liftIO $ print $ nest 2 $ (string (toNixName' name) <+> equals <+> text ("self." ++ show (nixAttr name version))) <> semi
     -- when (name /= toNixName' name) $
     --   liftIO $ print $ nest 2 $ (string name <+> equals <+> text ("self." ++ show (nixAttr name version))) <> semi
-    liftIO $ print $ nest 2 $ hang (string (nixAttr name version) <+> equals <+> text "callPackage") 2 (parens (disp nixExpr)) <+> (braces overrides <> semi)
+    liftIO $ print $ nest 2 $ hang (string (nixAttr name version) <+> equals <+> text "callPackage") 2 (parens (disp drv)) <+> (braces overrides <> semi)
     liftIO $ putStrLn ""
   liftIO $ putStrLn "}"
 
@@ -54,4 +66,9 @@ generatePackage (name, versions) = do
   let latestVersion = Set.findMax (Map.keysSet versions)
       pkgDescription = (Map.!) versions latestVersion
   srcSpec <- liftIO $ sourceFromHackage Nothing (name ++ "-" ++ display latestVersion)
-  return (name, latestVersion, (cabal2nix pkgDescription) { src = srcSpec })
+  let drv' = (cabal2nix pkgDescription) { src = srcSpec }
+  drv <- if revision drv' > 0
+           then do cabalFileHash <- liftIO $ getEditedCabalFileHash (pname drv') (version drv')
+                   return $ drv' { editedCabalFile = cabalFileHash }
+           else return drv'
+  return (name, latestVersion, drv)
