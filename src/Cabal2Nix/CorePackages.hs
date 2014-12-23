94ab72eb4389ca1a615896cd15c037b3856c8a7d
Peter Simons <simons@cryp.to>
2014-12-23 22:09:15 +0100

    Reduce set of filtered core packages to those that cannot be found on Hackage.

--- src/Cabal2Nix/CorePackages.hs
+++ src/Cabal2Nix/CorePackages.hs
@@ -8,36 +8,15 @@ module Cabal2Nix.CorePackages ( corePackages, coreBuildTools ) where
 -- The commented packages have previously been non-core, so we don't filter
 -- them.
 corePackages :: [String]
-corePackages = [
-    "array",
-    "base",
-    "bin-package-db",
-    "bytestring",
-    -- "Cabal",
-    "containers",
-    "directory",
-    -- "extensible-exceptions",
-    "ffi",
-    -- "filepath",
-    "ghc",
-    "ghc-binary",
-    "ghc-prim",
-    "haskell2010", -- new as core, but doesn't work in older GHCs anyway
-    "haskell98",
-    "hpc",
-    "integer-gmp",
-    "old-locale",
-    "old-time",
-    "pretty",
-    "process",
-    -- "random",
-    "template-haskell",
-    -- "time",
-    "unix"
+corePackages =
+  [ "ghc"
+  , "ghc-prim"
+  , "bin-package-db"
+  , "rts"
   ]
 
 coreBuildTools :: [String]
-coreBuildTools = [
-    "ghc",
-    "hsc2hs"
+coreBuildTools =
+  [ "ghc"
+  , "hsc2hs"
   ]
