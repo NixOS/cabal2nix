8a5ec344e72250b8ccdbb34a311148fcd0c23135
Peter Simons <simons@cryp.to>
2015-01-04 20:40:36 +0100

    Don't filter any core packages when generating an expression. We rely on 'null' overrides instead.

--- src/Cabal2Nix/CorePackages.hs
+++ src/Cabal2Nix/CorePackages.hs
@@ -4,16 +4,9 @@ module Cabal2Nix.CorePackages ( corePackages, coreBuildTools ) where
 -- nixpkgs. This should probably be configurable at first. Later, it might
 -- be good to actually include them as dependencies, but set them to null
 -- if GHC provides them (as different GHC versions vary).
---
--- The commented packages have previously been non-core, so we don't filter
--- them.
+
 corePackages :: [String]
-corePackages =
-  [ "ghc"
-  , "ghc-prim"
-  , "bin-package-db"
-  , "rts"
-  ]
+corePackages = []
 
 coreBuildTools :: [String]
 coreBuildTools =
