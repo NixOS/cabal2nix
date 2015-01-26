3226a8ac7ff05a37503beac9d8790da0718d8bf5
Peter Simons <simons@cryp.to>
2015-01-26 16:40:44 +0100

    Clean up Path type.

--- src/Language/Nix/Path.hs
+++ src/Language/Nix/Path.hs
@@ -1,13 +1,25 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
-module Language.Nix.Path where
+{-# LANGUAGE PatternGuards #-}
+module Language.Nix.Path ( Path, mkPath, path ) where
 
 import Control.Lens
+import Control.Applicative
 import Data.String
-import Distribution.Nix.Util.PrettyPrinting -- ( Pretty(..), text )
+import Distribution.Nix.Util.PrettyPrinting ( Pretty(..), text, punctuate, hcat, char )
 import Language.Nix.Identifier
 
 -- | Paths are non-empty lists of identifiers in Nix.
 
-newtype Path = Path [Identifier]
-  deriving (Show, Eq)
+newtype Path = Path { _segments :: [Identifier] }
+  deriving (Show, Eq, Ord)
+
+mkPath :: [Identifier] -> Path
+mkPath [] = error "Nix paths cannot be empty"
+mkPath p
+  | [Identifier s] <- p, needsQuoting s = error ("invalid Nix path: " ++ show p)
+  | otherwise                           = Path p
+
+path :: Lens' Path [Identifier]
+path f (Path ids) = mkPath `fmap` f ids
+
+instance Pretty Path where
+  pPrint p = hcat $ punctuate (char '.') $ pPrint <$> p^.path
