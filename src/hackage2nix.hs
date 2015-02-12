94989f149c65beb3ede3447a6169697ea9da02c6
Peter Simons <simons@cryp.to>
2015-02-12 21:10:17 +0100

    hackage2nix.hs: don't upgrade to blaze-builder 0.4.x yet

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -257,7 +257,8 @@ defaultConfiguration = Configuration
   , compilerId = CompilerId GHC (Version [7,8,4] [])
 
   , defaultPackageOverrides = map (\s -> fromMaybe (error (show s ++ " is not a valid override selector")) (simpleParse s))
-    [ "control-monad-free < 0.6"        -- newer versions don't compile with anything but GHC 7.8.x
+    [ "blaze-builder < 0.4"             -- the new version breaks many builds, like streaming-commons
+    , "control-monad-free < 0.6"        -- newer versions don't compile with anything but GHC 7.8.x
     , "mtl == 2.1.*"                    -- newer versions require transformers > 0.4.x, which we cannot provide in GHC 7.8.x
     , "HStringTemplate < 0.8"           -- 0.8 depends on time >= 1.5, which we cannot provide in GHC 7.8.x
     , "utf8-string < 1"                 -- version 1 breaks several packages right now, most notably hint
