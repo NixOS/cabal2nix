a8eaadbe6529cabd5088b8ae24fb325fc85a50c1
Peter Simons <simons@cryp.to>
2015-03-20 11:32:16 +0100

    hackage2nix.hs: don't update to QuickCheck 2.8 just yet.

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -261,6 +261,7 @@ defaultConfiguration = Configuration
     , "control-monad-free < 0.6"        -- newer versions don't compile with anything but GHC 7.8.x
     , "HStringTemplate < 0.8"           -- 0.8 depends on time >= 1.5, which we cannot provide in GHC 7.8.x
     , "mtl == 2.1.*"                    -- newer versions require transformers > 0.4.x, which we cannot provide in GHC 7.8.x
+    , "QuickCheck < 2.8"                -- 2.8 breaks chell-quickcheck-0.2.4
     , "utf8-string < 1"                 -- version 1 breaks several packages right now, most notably hint
     ]
 
