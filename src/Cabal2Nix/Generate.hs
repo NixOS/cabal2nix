92f56ca7904d6d270eb9dd4da0822ed9da959fde
Peter Simons <simons@cryp.to>
2015-04-23 14:39:18 +0200

    Update cabal2nix to generate output for GHC 7.10.x.

--- src/Cabal2Nix/Generate.hs
+++ src/Cabal2Nix/Generate.hs
@@ -21,7 +21,7 @@ cabal2nix flags' cabal = drv { cabalFlags = flags }
                             flags
                             (const True)
                             (Platform X86_64 Linux)                 -- shouldn't be hardcoded
-                            (unknownCompilerInfo (CompilerId GHC (Version [7,8,4] [])) NoAbiTag)
+                            (unknownCompilerInfo (CompilerId GHC (Version [7,10,1] [])) NoAbiTag)
                             []
                             cabal
 
