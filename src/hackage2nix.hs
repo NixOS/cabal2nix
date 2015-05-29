46e6e20ed73a407be281ca23d53db8d2fdc4cd41
Peter Simons <simons@cryp.to>
2015-05-29 11:23:32 +0200

    hackage2nix.hs: pipes-errors 0.2 is broken, too, so we might as well have the latest broken version

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -254,9 +254,9 @@ defaultConfiguration :: Configuration
 defaultConfiguration = Configuration
   { platform = Platform X86_64 Linux
   , compilerInfo = unknownCompilerInfo (CompilerId GHC (Version [7,10,1] [])) NoAbiTag
+
   , defaultPackageOverrides = map (\s -> fromMaybe (error (show s ++ " is not a valid override selector")) (simpleParse s))
     [ "errors < 2"                      -- the update breaks some ~40 important packages
-    , "pipes-errors < 0.3"              -- depends on errors 2.x
     , "th-orphans < 0.12.0"             -- https://github.com/bmillwood/haskell-src-meta/issues/43
     ]
 
