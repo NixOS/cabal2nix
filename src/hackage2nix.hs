1c5ac72f817d996a1ccec3e5a14496fb49806755
Peter Simons <simons@cryp.to>
2015-06-23 11:44:39 +0200

    hackage2nix: switch to latest GNUTLS

--- src/hackage2nix.hs
+++ src/hackage2nix.hs
@@ -259,7 +259,6 @@ defaultConfiguration = Configuration
     [ "errors < 2"                      -- the update breaks some ~40 important packages
     , "scotty < 0.10"                   -- newer versions break virtually all dependencies
     , "th-orphans < 0.12"               -- https://github.com/bmillwood/haskell-src-meta/issues/43
-    , "gnutls < 0.2"                    -- newer versions break network-protocol-xmpp-0.4.7
     , "cryptonite < 0.5"                -- https://github.com/vincenthz/hs-certificate/issues/54
     ]
 
