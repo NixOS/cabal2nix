763562e94687fc67312dbeea84fedac8ab00be6f
Peter Simons <simons@cryp.to>
2015-08-13 18:06:51 +0200

    Cabal2Nix/HackageGit.hs: re-write for improved performance

--- src/Cabal2Nix/HackageGit.hs
+++ src/Cabal2Nix/HackageGit.hs
@@ -1,101 +1,68 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Cabal2Nix.HackageGit ( Hackage, readHackage, module Data.Map )  where
 
-import Control.DeepSeq.Generics
-import GHC.Generics ( Generic )
 import Control.Monad
 import Data.Aeson
 import Data.ByteString.Char8 ( ByteString )
-import qualified Data.ByteString.Char8 as BS8 ( readFile )
+import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.Lazy.Char8 ( fromStrict )
 import Data.Digest.Pure.SHA ( sha256, showDigest )
+import Data.List ( foldl' )
 import Data.Map
 import Data.Maybe ( fromMaybe )
 import Data.String.UTF8 ( toString, fromRep )
-import Data.Version
-import Distribution.Package
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parse ( parsePackageDescription, ParseResult(..) )
-import Distribution.Text ( simpleParse, display )
+import Distribution.Text ( simpleParse )
+import Distribution.Version ( Version )
 import System.Directory
 import System.FilePath
-import Control.Monad.Par.Combinator
-import Control.Monad.Par.IO
-import Control.Monad.Trans ( liftIO )
-
--- | A 'Map' representation of the Hackage database. Every package name
--- maps to a non-empty set of version, and for every version there is a
--- Cabal file stored as a (lazy) 'ByteString'.
+import System.IO.Unsafe
 
 type Hackage = Map String (Map Version GenericPackageDescription)
+
+readHackage :: FilePath -> IO Hackage
+readHackage path = do db <- getSubDirs path >>= foldM discoverPackageVersions empty
+                      return $ mapWithKey (\pkg -> Data.List.foldl' (makeVersionMap pkg) empty) db
+  where
+    discoverPackageVersions :: Map String [String] -> String -> IO (Map String [String])
+    discoverPackageVersions db pkg = getSubDirs (path </> pkg) >>= \vs -> return (insert pkg vs db)
+
+    makeVersionMap :: String -> Map Version GenericPackageDescription -> String -> Map Version GenericPackageDescription
+    makeVersionMap pkg db v = insert (parseVersion v) (unsafePerformIO (readHackagePackage path pkg v)) db
+
+readHackagePackage :: FilePath -> String -> String -> IO GenericPackageDescription
+readHackagePackage path pkg version = do
+  let cabalFile = path </> pkg </> version </> pkg <.> "cabal"
+      metaFile = path </> pkg </> version </> pkg <.> "json"
+  buf <- BS.readFile cabalFile
+  meta <- parseMeta path version <$> BS.readFile metaFile
+  cabal <- case parsePackageDescription (decodeUTF8 buf) of
+    ParseOk _ a  -> return a
+    ParseFailed err -> fail $ cabalFile ++ ": " ++ show err
+  let cabal' = cabal { packageDescription = pd { customFieldsPD = cf } }
+      pd = packageDescription cabal
+      cf = customFieldsPD pd ++ ("X-Cabal-File-Hash", mkSHA256 buf)
+                             :  [ ("X-Package-" ++ k, v') | (k,v') <- toList (hashes meta) ]
+  return cabal'
+
+parseVersion :: String -> Version
+parseVersion str = fromMaybe (error $ "hackage-db: cannot parse version " ++ show str) (simpleParse str)
+
+decodeUTF8 :: ByteString -> String
+decodeUTF8 = toString . fromRep
 
 getSubDirs :: FilePath -> IO [FilePath]
 getSubDirs path = do
   let isDirectory p = doesDirectoryExist (path </> p)
   getDirectoryContents path >>= filterM isDirectory . Prelude.filter (\x -> head x /= '.')
 
--- | Read the Hackage database from the given 'FilePath' and return a
--- 'Hackage' map that provides fast access to its contents.
-
-readHackage :: FilePath -> IO Hackage
-readHackage root = runParIO $ do
-  pkglist <- liftIO $ getSubDirs root
-  hackage' <- parMapM (readHackagePackage root) $!! pkglist
-  let hackage = [ (pkg, Data.Map.mapWithKey (mkPkgDescr pkg) version2buf) | (pkg, version2buf) <- hackage' ]
-
-      mkPkgDescr :: String -> Version -> (ByteString, Meta) -> GenericPackageDescription
-      mkPkgDescr pkg v (buf,meta) = cabal { packageDescription = pd { customFieldsPD = cf } }
-        where
-          cabal = parsePackage pkg v buf
-          pd = packageDescription cabal
-          cf = customFieldsPD pd ++ ("X-Cabal-File-Hash", mkSHA256 buf)
-                                 :  [ ("X-Package-" ++ k, v') | (k,v') <- toList (hashes meta) ]
-
-      mkSHA256 :: ByteString -> String
-      mkSHA256 = showDigest . sha256 . fromStrict
-
-  return (fromList hackage)
-
-readHackagePackage :: FilePath -> FilePath -> ParIO (FilePath, Map Version (ByteString,Meta))
-readHackagePackage root pkg = do
-  versions <- liftIO $ getSubDirs (root </> pkg)
-  x <- parMapM (readHackagePackageVersion root pkg) $!! versions
-  return (pkg, fromList x)
-
-readHackagePackageVersion :: FilePath -> FilePath -> FilePath -> ParIO (Version, (ByteString,Meta))
-readHackagePackageVersion root pkg version = do
-  let v = pVersion version
-  cabal <- liftIO $ BS8.readFile (root </> pkg </> version </> pkg <.> "cabal")
-  meta <- liftIO (BS8.readFile (root </> pkg </> version </> pkg <.> "json"))
-  return (v, (cabal,parseMeta pkg v meta))
-
-pVersion :: String -> Version
-pVersion str = fromMaybe (error $ "hackage-db: cannot parse version " ++ show str) (simpleParse str)
-
--- | Convenience wrapper around 'parsePackage'' to parse a single Cabal
--- file. Failure is reported with 'error'.
-
-parsePackage :: String -> Version -> ByteString -> GenericPackageDescription
-parsePackage name version buf = case parsePackage' buf of
-                           Right a  -> a
-                           Left err -> error $ "cannot parse cabal package " ++ show name ++ "-" ++ display version ++ ": " ++ err
-
--- | Parse a single Cabal file.
-
-parsePackage' :: ByteString -> Either String GenericPackageDescription
-parsePackage' buf = case parsePackageDescription (decodeUTF8 buf) of
-                     ParseOk _ a     -> Right a
-                     ParseFailed err -> Left (show err)
-  where
-    decodeUTF8 :: ByteString -> String
-    decodeUTF8 = toString . fromRep
+mkSHA256 :: ByteString -> String
+mkSHA256 = showDigest . sha256 . fromStrict
 
 data Meta = Meta { hashes :: Map String String {-, locations :: [String], pkgsize :: Int-} }
-  deriving (Show, Generic)
-
-instance NFData Meta where rnf = genericRnf
+  deriving (Show)
 
 instance FromJSON Meta where
     parseJSON (Object v) = Meta <$>
@@ -105,10 +72,8 @@ instance FromJSON Meta where
     -- A non-Object value is of the wrong type, so fail.
     parseJSON _          = mzero
 
-parseMeta :: String -> Version -> ByteString -> Meta
+parseMeta :: String -> String -> ByteString -> Meta
 parseMeta pkg version buf =
   case eitherDecodeStrict buf of
-    Left msg -> error $ "hackage-db: cannot decode JSON meta data for " ++ display pkgid ++ ":" ++ msg
+    Left msg -> error $ "hackage-db: cannot decode JSON meta data for " ++ pkg ++ "-" ++ version ++ ":" ++ msg
     Right x -> x
-  where
-    pkgid = PackageIdentifier (PackageName pkg) version
