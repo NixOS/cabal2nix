ace1ee4216dcd412d900132de72fae2554949645
Peter Simons <simons@cryp.to>
2015-08-21 17:32:01 +0200

    Language.Nix: convenience module

---
+++ src/Language/Nix.hs
@@ -0,0 +1,9 @@
+module Language.Nix
+  ( module Language.Nix.Identifier
+  , module Language.Nix.Path
+  , module Language.Nix.Binding
+  ) where
+
+import Language.Nix.Identifier
+import Language.Nix.Path
+import Language.Nix.Binding
