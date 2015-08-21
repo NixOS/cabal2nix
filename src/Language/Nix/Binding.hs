6642731b59f72720da32148997c12b9c2557b815
Peter Simons <simons@cryp.to>
2015-08-21 17:33:04 +0200

    Binding.hs: add pretty printer

--- src/Language/Nix/Binding.hs
+++ src/Language/Nix/Binding.hs
@@ -1,12 +1,32 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Language.Nix.Binding ( Binding, localName, reference ) where
+module Language.Nix.Binding ( Binding, binding, localName, reference ) where
 
-import Control.Lens
+import Internal.Lens
 import Language.Nix.Identifier
 import Language.Nix.Path
+import Internal.PrettyPrinting
 
 data Binding = Bind { _localName :: Identifier, _reference :: Path }
   deriving (Show, Eq, Ord)
 
 makeLenses ''Binding
+
+binding :: Lens' Binding (Identifier,Path)
+binding f (Bind l r) = uncurry Bind `fmap` f (l, r)
+
+instance Default Binding where
+  def = Bind def def
+
+instance Pretty Binding where
+  pPrint b = case (init ps, last ps) of
+               ([], i') -> if i == i'
+                              then text "inherit" <+> pPrint i' <> semi
+                              else pPrint i <+> equals <+> pPrint p <> semi
+               (p', i') -> if i == i'
+                              then text "inherit" <+> parens (pPrint (create path p')) <+> pPrint i' <> semi
+                              else pPrint i <+> equals <+> pPrint p <> semi
+
+    where
+      (i, p) = view binding b
+      ps = view path p
