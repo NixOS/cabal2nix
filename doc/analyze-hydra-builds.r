9a7ccb6c15c7d7ebfb6dad0b32e9853534f257db
Peter Simons <simons@cryp.to>
2015-11-04 15:16:58 +0100

    doc: improve analyze-hydra-builds.r

--- doc/analyze-hydra-builds.r
+++ doc/analyze-hydra-builds.r
@@ -12,7 +12,7 @@ builds <- within(as.data.table(read.csv("builds.csv", header=T, stringsAsFactors
     timestamp <- as.POSIXct(timestamp, origin="1970-01-01")
     starttime <- as.POSIXct(starttime, origin="1970-01-01")
     stoptime <- as.POSIXct(stoptime, origin="1970-01-01")
-    runtime <- difftime(stoptime, starttime, units="secs")
+    runtime <- difftime(stoptime, starttime, units="mins")
     machine <- sub("^(hydra|root)@", "", machine)
 })
 builds[machine=="", machine:="localhost"]
@@ -21,12 +21,7 @@ setkey(builds, project, jobset, job, timestamp)
 ## Determine the average build runtime of all successful Haskell builds that
 ## ran on "work".
 
-hs <- builds[ project=="nixpkgs" & jobset %in% c("haskell-updates", "stackage")
-            | project %in% c( "cabal2nix", "funcmp", "hackage-db", "hledger", "hsdns"
-                            , "hspec", "hsyslog", "language-nix", "nix-paths"
-                            )
-            ,
-            ]
+hs <- builds[project=="nixpkgs" & jobset=="haskell-updates",]
 hs <- hs[machine=="work" & buildstatus==0 & system == "x86_64-linux" & runtime > 0,]
 hs <- hs[,job:=sub("\\.x86_64-linux$", "", job)]
 hs <- hs[,job:=sub("^haskell(ng)?Packages\\.", "", job)]
@@ -36,7 +31,6 @@ hs <- hs[!grep("haskell(-ng)?\\.compiler\\.", job),]
 hs <- hs[!grep("^ghc\\.ghc", job),]
 hs <- hs[!grep("ghc[0-9]+$", job),]
 hs <- hs[!duplicated(hs$drvpath)]
-
 hs <- hs[
         , list(builds=length(runtime),runtime=sum(runtime),runtime.median=median(runtime),runtime.mean=mean(runtime),runtime.sd=sd(runtime))
         , by=job
@@ -45,4 +39,16 @@ hs <- hs[builds>10,]
 head(hs[order(runtime.mean, decreasing=T)], n=30)
 head(hs[order(builds, decreasing=T)], n=20)
 summary(as.numeric(hs$runtime.mean,units="mins"))
-quantile(as.numeric(hs$runtime.mean,units="mins"), 0.81)
+quantile(as.numeric(hs$runtime.mean,units="mins"), 0.82)
+
+## Determine the number of builds per day.
+
+hs <- builds[ project=="nixpkgs" & jobset=="haskell-updates" & starttime >= as.POSIXct("2015-01-01")
+            , list(builds=length(timestamp))
+            , by=list(day=as.Date(timestamp))
+            ]
+hs <- merge(hs, data.table(day=seq(min(hs$day), max(hs$day), by="days"), ignore=0), by="day", all=T)
+hs[is.na(builds)] <- 0
+hs$ignore <- NULL
+summary(hs$builds)
+quantile(hs$builds, .8)
