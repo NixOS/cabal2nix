d9dda87e56af79102efea4beee27c16a2f20e10b
Peter Simons <simons@cryp.to>
2016-06-03 18:45:08 +0200

    numeric-qq: don't run this test suite

--- distribution-nixpkgs/src/Distribution/Nixpkgs/Haskell/FromCabal/PostProcess.hs
+++ distribution-nixpkgs/src/Distribution/Nixpkgs/Haskell/FromCabal/PostProcess.hs
@@ -80,6 +80,7 @@ hooks =
   , ("mwc-random", set doCheck False)
   , ("mysql", set (libraryDepends . system . contains (pkg "mysql")) True)
   , ("network-attoparsec", set doCheck False) -- test suite requires network access
+  , ("numeric-qq", set doCheck False) -- test suite doesn't finish even after 1+ days
   , ("pandoc", set jailbreak False) -- jailbreak-cabal break the build
   , ("pandoc >= 1.16.0.2", set doCheck False) -- https://github.com/jgm/pandoc/issues/2709 and https://github.com/fpco/stackage/issues/1332
   , ("pandoc-citeproc", set doCheck False) -- https://github.com/jgm/pandoc-citeproc/issues/172
