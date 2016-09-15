4decb9c3b9abdf3d27b3733401e15e44ccc146ae
Peter Simons <simons@cryp.to>
2016-09-15 18:50:11 +0200

    Add support for aeson 1.x.

--- src/Distribution/Nixpkgs/Haskell/FromCabal/Configuration.hs
+++ src/Distribution/Nixpkgs/Haskell/FromCabal/Configuration.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE CPP               #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -10,6 +10,7 @@ module Distribution.Nixpkgs.Haskell.FromCabal.Configuration ( Configuration(..),
 import Control.DeepSeq
 import Control.Lens
 import Control.Monad
+import Data.Aeson
 import Data.Map as Map
 import Data.Set as Set
 import Data.Text as T
@@ -65,24 +66,31 @@ instance FromJSON Identifier where
   parseJSON (String s) = pure (review ident (T.unpack s))
   parseJSON s = fail ("parseJSON: " ++ show s ++ " is not a valid Nix identifier")
 
--- aeson-0.11 introduces a general instance, so we just need a couple
--- of specific ones
-#if MIN_VERSION_aeson(0,11,0)
+#if MIN_VERSION_aeson(1,0,0)
+
+instance FromJSONKey Identifier where
+  fromJSONKey = FromJSONKeyText parseKey
+
+instance FromJSONKey PackageName where
+  fromJSONKey = FromJSONKeyText parseKey
+
+#elif MIN_VERSION_aeson(0,11,0)
+
 instance (FromJSON v) => FromJSON (Map Identifier v) where
   parseJSON = fmap (Map.mapKeys parseKey) . parseJSON
 
 instance (FromJSON v) => FromJSON (Map PackageName v) where
   parseJSON = fmap (Map.mapKeys parseKey) . parseJSON
 
-parseKey :: (FromJSON k) => Text -> k
-parseKey s = either error id (parseEither parseJSON (String s))
 #else
+
 instance (Ord k, FromJSON k, FromJSON v) => FromJSON (Map k v) where
   parseJSON  = fmap (Map.mapKeys parseKey) . parseJSON
-    where
-      parseKey :: FromJSON k => Text -> k
-      parseKey s = either error id (parseEither parseJSON (String s))
+
 #endif
+
+parseKey :: FromJSON k => Text -> k
+parseKey s = either error id (parseEither parseJSON (String s))
 
 readConfiguration :: FilePath -> IO Configuration
 readConfiguration path =
