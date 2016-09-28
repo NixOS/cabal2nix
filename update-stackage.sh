c0b597f10f5090e8d8f8fd24d5fa97c6095c65f0
Peter Simons <simons@cryp.to>
2016-09-28 18:51:12 +0200

    update-stackage.sh: drop versions restrictions on stack, git-annex, and cabal2nix

--- update-stackage.sh
+++ update-stackage.sh
@@ -8,6 +8,7 @@ trap "rm $tmpfile" 0
 curl -L -s "https://www.stackage.org/lts/cabal.config" >"$tmpfile"
 version=$(sed -n "s/^--.*http:..www.stackage.org.snapshot.lts-//p" "$tmpfile")
 
+# Create a simple yaml version of the file.
 sed -r \
     -e '/^--/d' \
     -e 's|^constraints:||' \
@@ -16,9 +17,17 @@ sed -r \
     -e '/installed$/d' \
     -i "$tmpfile"
 
+# Drop restrictions on some tools where we always want the latest version.
+sed -r \
+    -e '/ cabal2nix /d' \
+    -e '/ stack /d' \
+    -i "$tmpfile"
+
+# Drop the previous configuration ...
 sed -e '/# LTS Haskell/,/^$/c \TODO\
 '   -i nixpkgs/pkgs/development/haskell-modules/configuration-hackage2nix.yaml
 
+# ... and replace it with the new one.
 sed -e "/TODO/r $tmpfile" \
     -e "s/TODO/  # LTS Haskell $version/" \
     -i nixpkgs/pkgs/development/haskell-modules/configuration-hackage2nix.yaml
