12a842b2d2066924f139bdc5f43140b36f5b82dc
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-05-10 13:08:42 -0700

    Add failing regression test for issue #274.

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,31 @@
 module Main ( main ) where
 
 import Test.DocTest
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Cabal2nix ( mainWithArgs )
+import System.Exit ( ExitCode (..) )
+import System.FilePath ( (</>) )
+import System.Process ( readProcessWithExitCode )
 
 main :: IO ()
-main = doctest [ "-isrc", "src" ]
+main = doctest [ "-isrc", "src" ] >> defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [regressionTests]
+
+regressionTests :: TestTree
+regressionTests = testGroup "Regression"
+  [ testCase "#274: Generating an expression from a local file is broken since --subpath was added" $ do
+      parsec <- allCabalHashes "parsec/3.0.0/parsec.cabal"
+      mainWithArgs [ "--sha256=dontworry", parsec]
+  ]
+
+
+allCabalHashes :: FilePath -> IO String
+allCabalHashes cabalPath = do
+  (exit, out, err) <- readProcessWithExitCode "nix-build" ["--no-out-link", "<nixpkgs>", "-A", "all-cabal-hashes"] ""
+  case exit of
+    ExitSuccess   -> return $ filter (/= '\n') $ out </> cabalPath
+    ExitFailure _ -> error err
