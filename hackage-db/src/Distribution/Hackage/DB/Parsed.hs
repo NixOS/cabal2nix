4610def32f955348b99d1f8938f3bfbea580c82d
Peter Simons <simons@cryp.to>
2017-07-08 15:04:04 +0200

    Implement parsed data structure.

--- hackage-db/src/Distribution/Hackage/DB/Parsed.hs
+++ hackage-db/src/Distribution/Hackage/DB/Parsed.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 {- |
    Maintainer:  simons@cryp.to
    Stability:   provisional
@@ -6,60 +8,68 @@
 
 module Distribution.Hackage.DB.Parsed where
 
--- import Data.ByteString.Lazy.Char8 ( ByteString )
--- import Data.Map
--- import Data.String.UTF8 ( toString, fromRep )
--- import Data.Version
--- import qualified Distribution.Hackage.DB.Unparsed as Unparsed
--- import Distribution.PackageDescription
--- import Distribution.PackageDescription.Parse ( parsePackageDescription, ParseResult(..) )
--- import Distribution.Text ( display )
---
--- -- | A 'Map' representation of the Hackage database. Every package name
--- -- maps to a non-empty set of version, and for every version there is a
--- -- parsed Cabal file.
---
--- type Hackage = Map String (Map Version GenericPackageDescription)
---
--- -- | Read the Hackage database from the location determined by 'hackagePath'
--- -- and return a 'Map' that provides fast access to its contents.
---
--- readHackage :: IO Hackage
--- readHackage = fmap parseUnparsedHackage Unparsed.readHackage
---
--- -- | Read the Hackage database from the given 'FilePath' and return a
--- -- 'Hackage' map that provides fast access to its contents.
---
--- readHackage' :: FilePath -> IO Hackage
--- readHackage' = fmap parseUnparsedHackage . Unparsed.readHackage'
---
--- -- | Parse the contents of Hackage's @00-index.tar@ into a 'Hackage' map.
---
--- parseHackage :: ByteString -> Hackage
--- parseHackage = parseUnparsedHackage . Unparsed.parseHackage
---
--- -- | Convert an 'Unparsed.Hackage' map into a parsed 'Hackage' map.
---
--- parseUnparsedHackage :: Unparsed.Hackage -> Hackage
--- parseUnparsedHackage = Data.Map.mapWithKey parsePackages
---   where
---     parsePackages :: String -> Map Version ByteString -> Map Version GenericPackageDescription
---     parsePackages name = Data.Map.mapWithKey (parsePackage name)
---
--- -- | Convenience wrapper around 'parsePackage'' to parse a single Cabal
--- -- file. Failure is reported with 'error'.
---
--- parsePackage :: String -> Version -> ByteString -> GenericPackageDescription
--- parsePackage name version buf = case parsePackage' buf of
---                            Right a  -> a
---                            Left err -> error $ "cannot parse cabal package " ++ show name ++ "-" ++ display version ++ ": " ++ err
---
--- -- | Parse a single Cabal file.
---
--- parsePackage' :: ByteString -> Either String GenericPackageDescription
--- parsePackage' buf = case parsePackageDescription (decodeUTF8 buf) of
---                      ParseOk _ a     -> Right a
---                      ParseFailed err -> Left (show err)
---   where
---     decodeUTF8 :: ByteString -> String
---     decodeUTF8 = toString . fromRep
+import qualified Distribution.Hackage.DB.Unparsed as U
+import Distribution.Hackage.DB.Utility
+
+import Data.Aeson
+import Data.ByteString.Lazy.UTF8 as BS
+import Data.Map as Map
+import Distribution.Package
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.Version
+import GHC.Generics ( Generic )
+
+type HackageDB = Map PackageName PackageData
+
+data PackageData = PackageData { preferredVersions :: VersionRange
+                               , versions          :: Map Version VersionData
+                               }
+  deriving (Show)
+
+data VersionData = VersionData { cabalFile :: GenericPackageDescription
+                               , meta      :: MetaData
+                               }
+  deriving (Show)
+
+parseDB :: U.HackageDB -> HackageDB
+parseDB = Map.mapWithKey parsePackageData
+
+parsePackageData :: PackageName -> U.PackageData -> PackageData
+parsePackageData pn (U.PackageData pv vs) = PackageData vr (Map.mapWithKey (parseVersionData pn) vs)
+  where
+    Dependency _ vr = parseText "Dependency" (toString pv)
+
+parseVersionData :: PackageName -> Version -> U.VersionData -> VersionData
+parseVersionData pn v (U.VersionData cf m) = VersionData gpd md
+  where
+    gpd = case parsePackageDescription (toString cf) of
+            ParseOk _ a     -> a
+            ParseFailed msg -> error (show msg) -- TODO: define proper exception
+
+    md = case eitherDecode m of
+           Left msg -> error msg  -- TODO: create proper exception type
+           Right md' -> md'
+
+data MetaData = MetaData { signed :: SignedMetaData
+                         , signatures :: [String]
+                         }
+  deriving (Show, Generic)
+
+instance FromJSON MetaData
+
+data SignedMetaData = SignedMetaData { version :: Int
+                                     , expires :: Maybe String
+                                     , _type   :: String
+                                     , targets :: Map String TargetData
+                                     }
+  deriving (Show, Generic)
+
+instance FromJSON SignedMetaData
+
+data TargetData = TargetData { length :: Int
+                             , hashes :: Map String String
+                             }
+  deriving (Show, Generic)
+
+instance FromJSON TargetData
