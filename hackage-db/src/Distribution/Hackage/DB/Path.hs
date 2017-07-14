bcdd69b2d4664c74a7ef91a419a4fb9a0e91b626
Peter Simons <simons@cryp.to>
2017-07-14 13:53:45 +0200

    cosmetic

--- hackage-db/src/Distribution/Hackage/DB/Path.hs
+++ hackage-db/src/Distribution/Hackage/DB/Path.hs
@@ -17,11 +17,6 @@ import Control.Exception
 import System.Directory
 import System.FilePath
 
--- | Determine the default path of the Hackage database, which typically
--- resides at @"$HOME\/.cabal\/packages\/hackage.haskell.org\/00-index.tar"@.
--- Running the command @"cabal update"@ will keep that file up-to-date.
-
-
 cabalStateDir :: IO FilePath
 cabalStateDir = getAppUserDataDirectory "cabal"
 
@@ -33,14 +28,18 @@ cabalTarballDir repo = do
 hackageTarballDir :: IO FilePath
 hackageTarballDir = cabalTarballDir "hackage.haskell.org"
 
+-- | Determine the default path of the Hackage database, which typically
+-- resides at @"$HOME\/.cabal\/packages\/hackage.haskell.org\/00-index.tar"@.
+-- Running the command @"cabal update"@ will keep that file up-to-date.
+
 hackageTarball :: IO FilePath
 hackageTarball = do
   htd <- hackageTarballDir
   let idx00 = htd </> "00-index.tar"
       idx01 = htd </> "01-index.tar"
-  -- Yes, using 'msum' here instead of this convolted mess would be nice, but
-  -- unfortunetaly there was no reliable MonadPlus instance for IO in pre 8.x
-  -- versions of GHC. Se we use the ugly code for sake of portability.
+  -- Using 'msum' here would be nice, but unfortunetaly there was no reliable
+  -- MonadPlus instance for IO in pre 8.x versions of GHC. Se we use the ugly
+  -- code for sake of portability.
   have01 <- doesFileExist idx01
   if have01 then return idx01 else do
     have00 <- doesFileExist idx00
