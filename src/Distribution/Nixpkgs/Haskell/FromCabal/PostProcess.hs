0b2f94ab411bf2e1a9df675781e6cb2340cf7d8d
Peter Simons <simons@cryp.to>
2017-07-26 09:27:49 +0200

    lzma-clib: avoid evaluation on non-Windows platforms

--- src/Distribution/Nixpkgs/Haskell/FromCabal/PostProcess.hs
+++ src/Distribution/Nixpkgs/Haskell/FromCabal/PostProcess.hs
@@ -89,6 +89,7 @@ hooks =
   , ("libconfig", over (libraryDepends . system) (replace "config = null" (pkg "libconfig")))
   , ("liquid-fixpoint", set (executableDepends . system . contains (pkg "ocaml")) True . set (testDepends . system . contains (pkg "z3")) True)
   , ("liquidhaskell", set (testDepends . system . contains (pkg "z3")) True)
+  , ("lzma-clib", over (metaSection . platforms) (Set.filter (\(Platform _  os) -> os == Windows)) . set (libraryDepends . haskell . contains (bind "self.only-buildable-on-windows")) False)
   , ("MFlow < 4.6", set (libraryDepends . tool . contains (bind "self.cpphs")) True)
   , ("mwc-random", set doCheck False)
   , ("mysql", set (libraryDepends . system . contains (pkg "mysql")) True)
