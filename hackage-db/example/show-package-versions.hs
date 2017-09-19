25a8f0943a91ec57c05b3add01705592cd480e6b
Peter Simons <simons@cryp.to>
2017-09-19 20:52:01 +0200

    show-package-versions.hs: use cpp to support old Cabal versions

--- hackage-db/example/show-package-versions.hs
+++ hackage-db/example/show-package-versions.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE CPP #-}
+
+#if !MIN_VERSION_Cabal(2,0,0)
+#  define mkPackageName PackageName
+#endif
+
 module Main ( main ) where
 
 import Distribution.Hackage.DB
 
 import Control.Monad ( forM_ )
 import Data.Map as Map
-import Distribution.Package ( mkPackageName )
+import Distribution.Package
 import Distribution.Text ( display )
 import System.Environment ( getArgs )
 
