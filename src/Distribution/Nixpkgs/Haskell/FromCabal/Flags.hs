48a154e939369a5394f4e65cf481333eab0058e7
Peter Simons <simons@cryp.to>
2017-12-19 22:39:20 +0100

    cassava: disable bytestring--lt-0_10_4 flag to fix build in LTS-10.x

--- src/Distribution/Nixpkgs/Haskell/FromCabal/Flags.hs
+++ src/Distribution/Nixpkgs/Haskell/FromCabal/Flags.hs
@@ -12,6 +12,7 @@ configureCabalFlags :: PackageIdentifier -> FlagAssignment
 configureCabalFlags (PackageIdentifier name version)
  | name == "accelerate-examples"= [disable "opencl"]
  | name == "arithmoi"           = [disable "llvm"]
+ | name == "cassava"            = [disable "bytestring--lt-0_10_4"]
  | name == "darcs"              = [enable "library", enable "force-char8-encoding"]
  | name == "diagrams-builder"   = [enable "cairo", enable "svg", enable "ps", enable "rasterific"]
  | name == "folds"              = [disable "test-hlint"]
