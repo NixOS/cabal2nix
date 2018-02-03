7642d2512f5a99a39fcf14b8d75326008a033e3f
Peter Simons <simons@cryp.to>
2018-02-03 19:47:25 +0100

    BuildInfo: add a Semigroup instance to be compatible with GHC 8.4.x

--- src/Distribution/Nixpkgs/Haskell/BuildInfo.hs
+++ src/Distribution/Nixpkgs/Haskell/BuildInfo.hs
@@ -11,11 +11,12 @@ module Distribution.Nixpkgs.Haskell.BuildInfo
 
 import Control.DeepSeq
 import Control.Lens
+import Data.Semigroup
 import Data.Set ( Set )
 import Data.Set.Lens
 import GHC.Generics ( Generic )
 import Language.Nix
-import Language.Nix.PrettyPrinting
+import Language.Nix.PrettyPrinting hiding ( (<>) )
 
 data BuildInfo = BuildInfo
   { _haskell   :: Set Binding
@@ -30,9 +31,13 @@ makeLenses ''BuildInfo
 instance Each BuildInfo BuildInfo (Set Binding) (Set Binding) where
   each f (BuildInfo a b c d) = BuildInfo <$> f a <*> f b <*> f c <*> f d
 
+instance Semigroup BuildInfo where
+  BuildInfo w1 x1 y1 z1 <> BuildInfo w2 x2 y2 z2 =
+    BuildInfo (w1 <> w2) (x1 <> x2) (y1 <> y2) (z1 <> z2)
+
 instance Monoid BuildInfo where
   mempty = BuildInfo mempty mempty mempty mempty
-  BuildInfo w1 x1 y1 z1 `mappend` BuildInfo w2 x2 y2 z2 = BuildInfo (w1 `mappend` w2) (x1 `mappend` x2) (y1 `mappend` y2) (z1 `mappend` z2)
+  mappend = (<>)
 
 instance NFData BuildInfo
 
