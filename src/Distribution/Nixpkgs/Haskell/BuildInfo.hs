9ecb1e7520490518827261a5d677b164177ddb51
Peter Simons <simons@cryp.to>
2018-04-26 08:53:50 +0200

    Avoid compiler warnings about unused Semigroup import.

--- src/Distribution/Nixpkgs/Haskell/BuildInfo.hs
+++ src/Distribution/Nixpkgs/Haskell/BuildInfo.hs
@@ -11,7 +11,7 @@ module Distribution.Nixpkgs.Haskell.BuildInfo
 
 import Control.DeepSeq
 import Control.Lens
-import Data.Semigroup
+import Data.Semigroup as Sem
 import Data.Set ( Set )
 import Data.Set.Lens
 import GHC.Generics ( Generic )
@@ -31,13 +31,13 @@ makeLenses ''BuildInfo
 instance Each BuildInfo BuildInfo (Set Binding) (Set Binding) where
   each f (BuildInfo a b c d) = BuildInfo <$> f a <*> f b <*> f c <*> f d
 
-instance Semigroup BuildInfo where
+instance Sem.Semigroup BuildInfo where
   BuildInfo w1 x1 y1 z1 <> BuildInfo w2 x2 y2 z2 =
     BuildInfo (w1 <> w2) (x1 <> x2) (y1 <> y2) (z1 <> z2)
 
 instance Monoid BuildInfo where
   mempty = BuildInfo mempty mempty mempty mempty
-  mappend = (<>)
+  mappend = (Sem.<>)
 
 instance NFData BuildInfo
 
