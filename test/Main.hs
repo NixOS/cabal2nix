95c25b73813ae9f1c170e714e50ad809b56bf5ba
Peter Simons <simons@cryp.to>
2018-06-28 16:14:14 +0200

    test: run the same tests with both the cabal2nix library and the executable

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main ( main ) where
 
-import Distribution.Nixpkgs.Haskell.FromCabal
-import Distribution.Nixpkgs.Haskell.Derivation
 import Distribution.Nixpkgs.Fetch
+import Distribution.Nixpkgs.Haskell.Derivation
+import Distribution.Nixpkgs.Haskell.FromCabal
+import Distribution.Nixpkgs.Haskell.FromCabal.Flags
 
 import Control.Lens
 import Control.Monad
+import qualified Data.Set as Set
 import Distribution.Compiler
+import Distribution.Package
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parsec
 import Distribution.System
@@ -15,6 +20,7 @@ import Distribution.Version
 import Language.Nix
 import System.Directory
 import System.FilePath
+import System.Process
 import Test.Tasty
 import Test.Tasty.Golden
 import Text.PrettyPrint.HughesPJClass
@@ -23,36 +29,65 @@ main :: IO ()
 main = do
   -- TODO: run this test with all kinds of setLocaleEncoding values to ensure we don't
   -- depend on the system environment: https://github.com/NixOS/cabal2nix/issues/333
+  cabal2nix <- findExecutable "cabal2nix" >>= maybe (fail "cannot find 'cabal2nix' in $PATH") return
   testCases <- listDirectoryFilesBySuffix ".cabal" "test/golden-test-cases"
-  defaultMain $ testGroup "regression-tests" (map regressionTest testCases)
+  defaultMain $ testGroup "regression-tests"
+    [ testGroup "cabal2nix library" (map testLibrary testCases)
+    , testGroup "cabal2nix executable" (map (testExecutable cabal2nix) testCases)
+    ]
 
-regressionTest :: String -> TestTree
-regressionTest cabalFile = do
+testLibrary :: String -> TestTree
+testLibrary cabalFile = do
   let nixFile = cabalFile `replaceExtension` "nix"
       goldenFile = nixFile `addExtension` "golden"
 
       cabal2nix :: GenericPackageDescription -> Derivation
       cabal2nix gpd = fromGenericPackageDescription
-                        (const True)
-                        (\i -> Just (binding # (i, path # [i])))
-                        (Platform X86_64 Linux)
-                        (unknownCompilerInfo (CompilerId GHC (mkVersion [8,2])) NoAbiTag)
-                        mempty
-                        []
-                        gpd
-                      & src .~ DerivationSource
-                                 { derivKind     = "url"
-                                 , derivUrl      = "http://example.org/"
-                                 , derivRevision = ""
-                                 , derivHash     = "abc"
-                                 }
+                         (const True)
+                         (\i -> Just (binding # (i, path # [i])))
+                         (Platform X86_64 Linux)
+                         (unknownCompilerInfo (CompilerId GHC (mkVersion [8,2])) NoAbiTag)
+                         (configureCabalFlags (packageId gpd))
+                         []
+                         gpd
+                       & src .~ DerivationSource
+                                  { derivKind     = "url"
+                                  , derivUrl      = "mirror://hackage/foo.tar.gz"
+                                  , derivRevision = ""
+                                  , derivHash     = "deadbeef"
+                                  }
+                       & extraFunctionArgs %~ Set.union (Set.singleton "inherit stdenv")
   goldenVsFileDiff
     nixFile
     (\ref new -> ["diff", "-u", ref, new])
     goldenFile
     nixFile
-    (readGenericPackageDescription silent cabalFile >>= writeFile nixFile . prettyShow . cabal2nix)
+    (readGenericPackageDescription silent cabalFile >>= writeFileLn nixFile . prettyShow . cabal2nix)
 
+-- | TODO:
+--
+--  * Run cabal2nix in an empty environment. Currently, this fails because the
+--    tool uses $HOME to write cache files for SHA256 hashes. To fix this, use
+--    of the cache directory ought to be optional. Arguably, the whole notion
+--    of caching those hashes is a bit silly in the presense of format 01
+--    cabal-install tarballs which contain them already.
+--
+--  * If we cannot get along without $HOME, then we should at least set it to
+--    some temporary location that is empty.
+--
+--  * Run this test with different kinds of encodings.
+
+
+testExecutable :: FilePath -> FilePath -> TestTree
+testExecutable exe cabalFile = do
+  let nixFile = cabalFile `replaceExtension` "nix"
+      goldenFile = nixFile `addExtension` "golden"
+  goldenVsFileDiff
+    nixFile
+    (\ref new -> ["diff", "-u", ref, new])
+    goldenFile
+    nixFile
+    (callCommand (unwords ["env LANG=en_US.UTF-8", exe, "--sha256=deadbeef", "--compiler=ghc-8.2", "--", cabalFile, ">"++nixFile]))
 
 -------------------------------------------------------------------------------
 -- * Helper functions
@@ -80,3 +115,10 @@ listDirectoryFiles dirPath = do
 listDirectoryFilesBySuffix :: String -> FilePath -> IO [FilePath]
 listDirectoryFilesBySuffix suff =
   fmap (filter ((suff ==) . takeExtension)) . listDirectoryFiles
+
+
+-- | A variant of 'writeFile' that appends a newline to the end of the buffer
+-- before writing.
+
+writeFileLn :: FilePath -> String -> IO ()
+writeFileLn p buf = writeFile p (buf ++ "\n")
