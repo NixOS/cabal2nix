c6f91ab9b186d940207c74240ea9c162cef4c53e
Peter Simons <simons@cryp.to>
2018-06-28 14:47:44 +0200

    regression-test: ensure that our test suite can run with 1K stack space again

--- test/Main.hs
+++ test/Main.hs
@@ -5,6 +5,7 @@ import Distribution.Nixpkgs.Haskell.Derivation
 import Distribution.Nixpkgs.Fetch
 
 import Control.Lens
+import Control.Monad
 import Distribution.Compiler
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parsec
@@ -12,6 +13,7 @@ import Distribution.System
 import Distribution.Verbosity
 import Distribution.Version
 import Language.Nix
+import System.Directory
 import System.FilePath
 import Test.Tasty
 import Test.Tasty.Golden
@@ -21,7 +23,7 @@ main :: IO ()
 main = do
   -- TODO: run this test with all kinds of setLocaleEncoding values to ensure we don't
   -- depend on the system environment: https://github.com/NixOS/cabal2nix/issues/333
-  testCases <- findByExtension [".cabal"] "test/golden-test-cases"
+  testCases <- listDirectoryFilesBySuffix ".cabal" "test/golden-test-cases"
   defaultMain $ testGroup "regression-tests" (map regressionTest testCases)
 
 regressionTest :: String -> TestTree
@@ -50,3 +52,31 @@ regressionTest cabalFile = do
     goldenFile
     nixFile
     (readGenericPackageDescription silent cabalFile >>= writeFile nixFile . prettyShow . cabal2nix)
+
+
+-------------------------------------------------------------------------------
+-- * Helper functions
+-------------------------------------------------------------------------------
+
+-- | Find all *files* in the given directory. This function does not recurse,
+-- nor does it include the special entries @'.'@ or @'..'@ in the result.
+
+listDirectoryFiles :: FilePath -> IO [FilePath]
+listDirectoryFiles dirPath = do
+  entries <- listDirectory dirPath
+  filterM doesFileExist (map (dirPath </>) entries)
+
+-- | Find all *files* in the given directory that end with the given suffix.
+-- This function does not recurse, nor does it include the special entries
+-- @'.'@ or @'..'@ in the result.
+--
+-- "Test.Tasty.Golden" offers 'findByExtension', which does something similar
+-- (and arguably more general) as those two functions, but unfortunately that
+-- function recurses through the directory hierarchy and in the process needs
+-- plenty of stack space. There might be a space leak in that code, I am not
+-- sure. Anyhow, since our test suite is supposed to run with 1K stack space,
+-- we prefer our own simpler code.
+
+listDirectoryFilesBySuffix :: String -> FilePath -> IO [FilePath]
+listDirectoryFilesBySuffix suff =
+  fmap (filter ((suff ==) . takeExtension)) . listDirectoryFiles
