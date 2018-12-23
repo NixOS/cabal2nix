fcd7cd0071a4a0b5d30f76e1968f0d5b92478537
Peter Simons <simons@cryp.to>
2018-12-23 14:36:04 +0100

    Move callback API into Distribution.Hackage.DB.Builder.

--- hackage-db/new-callback-api.hs
+++ hackage-db/new-callback-api.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
-{-# LANGUAGE BangPatterns #-}
 
 {- |
    Maintainer:  simons@cryp.to
@@ -10,16 +9,11 @@
 
 module Main ( main )  where
 
-import Distribution.Hackage.DB.Errors
-import Distribution.Hackage.DB.Path
-import Distribution.Hackage.DB.Utility
+import Distribution.Hackage.DB.Builder
 
-import Codec.Archive.Tar as Tar
-import Codec.Archive.Tar.Entry as Tar
 import Control.DeepSeq
 import Control.Exception
 import Control.Monad
-import Control.Monad.Catch
 import qualified Data.ByteString as BSS
 import qualified Data.ByteString.Lazy as BSL
 import Data.Map.Strict ( Map )
@@ -27,36 +21,6 @@ import qualified Data.Map.Strict as Map
 import Distribution.Types.PackageName
 import Distribution.Types.Version
 import GHC.Generics ( Generic )
-import System.FilePath
-
-readHackageTarball :: IO (Entries FormatError)
-readHackageTarball = hackageTarball >>= readTarball
-
-readTarball :: FilePath -> IO (Entries FormatError)
-readTarball = fmap Tar.read . BSL.readFile
-
-data Builder m a = Builder
-  { insertPreferredVersions :: PackageName -> EpochTime -> BSL.ByteString -> a -> m a
-  , insertCabalFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
-  , insertMetaFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
-  }
-
-parseTarball :: MonadThrow m => Builder m a -> Maybe EpochTime -> Entries FormatError -> a -> m a
-parseTarball b (Just et) (Next e es) !db = if entryTime e > et then return db else insertEntry b e db >>= parseTarball b (Just et) es
-parseTarball b Nothing   (Next e es) !db = insertEntry b e db >>= parseTarball b Nothing es
-parseTarball _ _         (Fail err)  _   = throwM err
-parseTarball _ _         Done        !db = return db
-
-insertEntry :: MonadThrow m => Builder m a -> Entry -> a -> m a
-insertEntry b e db =
-  case (splitDirectories (entryPath e), entryContent e) of
-    ([pn,"preferred-versions"], NormalFile buf _) -> insertPreferredVersions b (mkPackageName pn) (entryTime e) buf db
-    ([pn,v,file],               NormalFile buf _)
-      | takeExtension file == ".cabal"            -> insertCabalFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
-      | takeExtension file == ".json"             -> insertMetaFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
-    _                                             -> throwM (UnsupportedTarEntry e)
-
------ Test Code
 
 main :: IO ()
 main = do
---
+++ hackage-db/src/Distribution/Hackage/DB/Builder.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE BangPatterns #-}
+
+{- |
+   Maintainer:  simons@cryp.to
+   Stability:   provisional
+   Portability: portable
+ -}
+
+module Distribution.Hackage.DB.Builder
+  ( readHackageTarball, readTarball
+  , Builder(..), parseTarball
+  )
+  where
+
+import Distribution.Hackage.DB.Errors
+import Distribution.Hackage.DB.Path
+import Distribution.Hackage.DB.Utility
+
+import Codec.Archive.Tar as Tar
+import Codec.Archive.Tar.Entry as Tar
+import Control.Monad.Catch
+import qualified Data.ByteString.Lazy as BSL
+import Distribution.Types.PackageName
+import Distribution.Types.Version
+import System.FilePath
+
+readHackageTarball :: IO (Entries FormatError)
+readHackageTarball = hackageTarball >>= readTarball
+
+readTarball :: FilePath -> IO (Entries FormatError)
+readTarball = fmap Tar.read . BSL.readFile
+
+data Builder m a = Builder
+  { insertPreferredVersions :: PackageName -> EpochTime -> BSL.ByteString -> a -> m a
+  , insertCabalFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
+  , insertMetaFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
+  }
+
+parseTarball :: MonadThrow m => Builder m a -> Maybe EpochTime -> Entries FormatError -> a -> m a
+parseTarball b (Just et) (Next e es) !db = if entryTime e > et then return db else insertEntry b e db >>= parseTarball b (Just et) es
+parseTarball b Nothing   (Next e es) !db = insertEntry b e db >>= parseTarball b Nothing es
+parseTarball _ _         (Fail err)  _   = throwM err
+parseTarball _ _         Done        !db = return db
+
+insertEntry :: MonadThrow m => Builder m a -> Entry -> a -> m a
+insertEntry b e db =
+  case (splitDirectories (entryPath e), entryContent e) of
+    ([pn,"preferred-versions"], NormalFile buf _) -> insertPreferredVersions b (mkPackageName pn) (entryTime e) buf db
+    ([pn,v,file],               NormalFile buf _)
+      | takeExtension file == ".cabal"            -> insertCabalFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
+      | takeExtension file == ".json"             -> insertMetaFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
+    _                                             -> throwM (UnsupportedTarEntry e)
