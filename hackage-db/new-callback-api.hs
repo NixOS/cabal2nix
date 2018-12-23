1975c830988eabea0a401a0a5fef17d60dfcbe16
Peter Simons <simons@cryp.to>
2018-12-23 13:32:07 +0100

    new-callback-api: simplify the code to work without MonadState

--- hackage-db/new-callback-api.hs
+++ hackage-db/new-callback-api.hs
@@ -18,14 +18,14 @@ import Codec.Archive.Tar.Entry as Tar
 import Control.DeepSeq
 import Control.Exception
 import Control.Monad
-import Control.Monad.State.Strict
+import Control.Monad.Catch
 import qualified Data.ByteString as BSS
 import qualified Data.ByteString.Lazy as BSL
 import Data.Map.Strict ( Map )
 import qualified Data.Map.Strict as Map
-import GHC.Generics ( Generic )
 import Distribution.Types.PackageName
 import Distribution.Types.Version
+import GHC.Generics ( Generic )
 import System.FilePath
 
 readHackageTarball :: IO (Entries FormatError)
@@ -34,35 +34,36 @@ readHackageTarball = hackageTarball >>= readTarball
 readTarball :: FilePath -> IO (Entries FormatError)
 readTarball = fmap Tar.read . BSL.readFile
 
-data Builder m = Builder
-  { consumePreferredVersions :: PackageName -> EpochTime -> BSL.ByteString -> m ()
-  , consumeCabalFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> m ()
-  , consumeMetaFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> m ()
-  , consumeError :: SomeException -> m ()
+data Builder m a = Builder
+  { insertPreferredVersions :: PackageName -> EpochTime -> BSL.ByteString -> a -> m a
+  , insertCabalFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
+  , insertMetaFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
   }
 
-parseTarball :: Applicative m => Builder m -> Maybe EpochTime -> Entries FormatError -> m ()
-parseTarball b (Just et) (Next e es) = unless (entryTime e > et) (consumeEntry b e *> parseTarball b (Just et) es)
-parseTarball b Nothing   (Next e es) = consumeEntry b e *> parseTarball b Nothing es
-parseTarball b _         (Fail err)  = consumeError b (toException err)
-parseTarball _ _         Done        = pure ()
+parseTarball :: MonadThrow m => Builder m a -> Maybe EpochTime -> Entries FormatError -> a -> m a
+parseTarball b (Just et) (Next e es) db = if entryTime e > et then return db else insertEntry b e db >>= parseTarball b (Just et) es
+parseTarball b Nothing   (Next e es) db = insertEntry b e db >>= parseTarball b Nothing es
+parseTarball _ _         (Fail err)  _  = throwM err
+parseTarball _ _         Done        db = return db
 
-consumeEntry :: Builder m -> Entry -> m ()
-consumeEntry b e =
+insertEntry :: MonadThrow m => Builder m a -> Entry -> a -> m a
+insertEntry b e db =
   case (splitDirectories (entryPath e), entryContent e) of
-    ([pn,"preferred-versions"], NormalFile buf _) -> consumePreferredVersions b (mkPackageName pn) (entryTime e) buf
+    ([pn,"preferred-versions"], NormalFile buf _) -> insertPreferredVersions b (mkPackageName pn) (entryTime e) buf db
     ([pn,v,file],               NormalFile buf _)
-      | takeExtension file == ".cabal"            -> consumeCabalFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf
-      | takeExtension file == ".json"             -> consumeMetaFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf
-    _                                             -> consumeError b (toException (UnsupportedTarEntry e))
+      | takeExtension file == ".cabal"            -> insertCabalFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
+      | takeExtension file == ".json"             -> insertMetaFile b (mkPackageName pn) (parseText "Version" v) (entryTime e) buf db
+    _                                             -> throwM (UnsupportedTarEntry e)
 
 ----- Test Code
 
 main :: IO ()
 main = do
   es <- readHackageTarball
-  db1 <- execStateT (parseTarball hackageDbBuilder Nothing es) mempty
-  let db2 = execState (parseTarball hackageDbBuilder Nothing es) mempty
+  db1 <- parseTarball hackageDbBuilder Nothing es (mempty :: HackageDB)
+  let db2 = case parseTarball hackageDbBuilder Nothing es mempty of
+              Left e -> error (show (e :: SomeException))
+              Right db -> db
   unless (db1 == db2) $
     fail "This is not supposed to happen."
 
@@ -80,23 +81,22 @@ data PackageVersionData = PackageVersionData
   }
   deriving (Show, Eq, Generic, NFData)
 
-hackageDbBuilder :: MonadState HackageDB m => Builder m
+hackageDbBuilder :: Monad m => Builder m HackageDB
 hackageDbBuilder = Builder
-  { consumePreferredVersions = \pn _ buf -> let new     = PackageData mempty (BSL.toStrict buf)
-                                                f old _ = old { preferredVersions = preferredVersions new }
-                                            in modify (Map.insertWith f pn new)
+  { insertPreferredVersions = \pn _ buf -> let new     = PackageData mempty (BSL.toStrict buf)
+                                               f old _ = old { preferredVersions = preferredVersions new }
+                                           in return . Map.insertWith f pn new
 
-  , consumeCabalFile         = \pn v _ buf -> let f Nothing   = PackageData (Map.singleton v new) mempty
-                                                  f (Just pd) = pd { versions = Map.insertWith g v new (versions pd) }
-                                                  new         = PackageVersionData (BSL.toStrict buf) mempty
-                                                  g old _     = old { cabalFile = cabalFile new }
-                                              in modify (Map.alter (Just . f) pn)
+  , insertCabalFile         = \pn v _ buf -> let f Nothing   = PackageData (Map.singleton v new) mempty
+                                                 f (Just pd) = pd { versions = Map.insertWith g v new (versions pd) }
+                                                 new         = PackageVersionData (BSL.toStrict buf) mempty
+                                                 g old _     = old { cabalFile = cabalFile new }
+                                             in return . Map.alter (Just . f) pn
 
-  , consumeMetaFile          = \pn v _ buf -> let f Nothing   = PackageData (Map.singleton v new) mempty
-                                                  f (Just pd) = pd { versions = Map.insertWith g v new (versions pd) }
+  , insertMetaFile          = \pn v _ buf -> let f Nothing   = PackageData (Map.singleton v new) mempty
+                                                 f (Just pd) = pd { versions = Map.insertWith g v new (versions pd) }
 
-                                                  new         = PackageVersionData mempty (BSL.toStrict buf)
-                                                  g old _     = old { metaFile = metaFile new }
-                                              in modify (Map.alter (Just . f) pn)
-  , consumeError             = fail . show
+                                                 new         = PackageVersionData mempty (BSL.toStrict buf)
+                                                 g old _     = old { metaFile = metaFile new }
+                                             in return . Map.alter (Just . f) pn
   }
