5a69cd5fa25adb01fa70d15f27565d267981548b
Peter Simons <simons@cryp.to>
2018-12-23 14:49:30 +0100

    Make parseTarball inlinable to ensure that ghc specializes the function, i.e. to IO.

--- hackage-db/src/Distribution/Hackage/DB/Builder.hs
+++ hackage-db/src/Distribution/Hackage/DB/Builder.hs
@@ -36,12 +36,14 @@ data Builder m a = Builder
   , insertMetaFile :: PackageName -> Version -> EpochTime -> BSL.ByteString -> a -> m a
   }
 
+{-# INLINABLE parseTarball #-}
 parseTarball :: MonadThrow m => Builder m a -> Maybe EpochTime -> Entries FormatError -> a -> m a
 parseTarball b (Just et) (Next e es) !db = if entryTime e > et then return db else insertEntry b e db >>= parseTarball b (Just et) es
 parseTarball b Nothing   (Next e es) !db = insertEntry b e db >>= parseTarball b Nothing es
 parseTarball _ _         (Fail err)  _   = throwM err
 parseTarball _ _         Done        !db = return db
 
+{-# INLINABLE insertEntry #-}
 insertEntry :: MonadThrow m => Builder m a -> Entry -> a -> m a
 insertEntry b e db =
   case (splitDirectories (entryPath e), entryContent e) of
