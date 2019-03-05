50125218b2a2bdafe6bbebf28626b85e73900d51
Peter Simons <simons@cryp.to>
2019-03-05 21:01:19 +0100

    Update name mapping and resolver lists to cope with disappeared aliases.
    See https://github.com/NixOS/cabal2nix/pull/411#issuecomment-469747125 and
    following comments for details.

--- src/Distribution/Nixpkgs/Haskell/FromCabal.hs
+++ src/Distribution/Nixpkgs/Haskell/FromCabal.hs
@@ -139,7 +139,7 @@ fromPackageDescription haskellResolver nixpkgsResolver missingDeps flags Package
       | i == "gtk2"                      = binding # (i, path # ["pkgs","gtk2"])               -- TODO: these cases should not be necessary
       | i == "gtk3"                      = binding # (i, path # ["pkgs","gtk3"])
       | i == "gtksourceview3"            = binding # (i, path # ["pkgs","gtksourceview3"])
-      | i == "vte_291"                  = binding # (i, path # ["pkgs","vte"])
+      | i == "vte_291"                   = binding # (i, path # ["pkgs","vte"])
       | Just p <- nixpkgsResolver i, init (view (reference . path) p) `Set.member` goodScopes = p
       | otherwise                        = bindNull i
 
