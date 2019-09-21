4c951bceec79adb465792d3e93c47cfec378234a
Peter Simons <simons@cryp.to>
2019-09-21 15:59:27 +0200

    Fix doctests.
    Relying on the exact error message returned by aeson is a bad idea.

--- hackage-db/src/Distribution/Hackage/DB/MetaData.hs
+++ hackage-db/src/Distribution/Hackage/DB/MetaData.hs
@@ -30,10 +30,6 @@ import GHC.Generics ( Generic )
 -- >>> :set -XOverloadedStrings
 -- >>> parseMetaData "{\"signatures\":[],\"signed\":{\"_type\":\"Targets\",\"expires\":null,\"targets\":{\"<repo>/package/jailbreak-cabal-1.3.2.tar.gz\":{\"hashes\":{\"md5\":\"ba42b3e68323ccbeb3ac900cd68f9e90\",\"sha256\":\"212a8bbc3dfc748c4063282414a2726709d651322f3984c9989179d2352950f4\"},\"length\":2269}},\"version\":0}}"
 -- MetaData {signed = SignedMetaData {version = 0, expires = Nothing, _type = "Targets", targets = fromList [("<repo>/package/jailbreak-cabal-1.3.2.tar.gz",TargetData {length = 2269, hashes = fromList [("md5","ba42b3e68323ccbeb3ac900cd68f9e90"),("sha256","212a8bbc3dfc748c4063282414a2726709d651322f3984c9989179d2352950f4")]})]}, signatures = []}
--- >>> parseMetaData "{}"
--- *** Exception: InvalidMetaFile "Error in $: key \"signed\" not present"
--- >>> parseMetaData ""
--- *** Exception: InvalidMetaFile "Error in $: not enough input"
 
 parseMetaData :: ByteString -> MetaData
 parseMetaData = either (throw . InvalidMetaFile) id . eitherDecode
