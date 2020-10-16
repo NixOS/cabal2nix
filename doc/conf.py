d349a0ebe4dd615577251b8997a5fa15f1233a4e
Peter Simons <simons@cryp.to>
2020-10-16 12:53:37 +0200

    doc: configure sphinx to use 'index' as master document

--- doc/conf.py
+++ doc/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The document name of the “master” document, that is, the document that contains
+# the root toctree directive.
+master_doc = 'index'
