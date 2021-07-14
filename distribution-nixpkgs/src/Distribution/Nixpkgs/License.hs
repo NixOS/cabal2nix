0d488d83dbe0147f2be022245cdfbe88273c6c4f
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-07-14 16:33:15 +0200

    Distribution.Nixpkgs.License: fix minor issues and split into paragraphs
    * pkgs/lib/licenses.nix -> lib/licenses.nix
    
    * Incorrect reference to a Text type class (?) and usage of display
      instead of prettyShow
    
    * Convert examples into REPL sections, so they are picked up by doctest

--- distribution-nixpkgs/src/Distribution/Nixpkgs/License.hs
+++ distribution-nixpkgs/src/Distribution/Nixpkgs/License.hs
@@ -2,8 +2,8 @@
 
 {- |
    Known licenses in Nix expressions are represented using the
-   attributes defined in @pkgs\/lib\/licenses.nix@, and unknown licenses
-   are represented as a literal string.
+   attributes defined in nixpkgs' @lib\/licenses.nix@, and
+   unknown licenses are represented as a literal string.
  -}
 
 module Distribution.Nixpkgs.License ( License(..) ) where
@@ -14,27 +14,28 @@ import GHC.Generics ( Generic )
 import Language.Nix.PrettyPrinting
 
 -- | The representation for licenses used in Nix derivations. Known
--- licenses are Nix expressions -- such as @lib.licenses.bsd3@
--- --, so their exact \"name\" is not generally known, because the path
--- to @lib@ depends on the context defined in the expression. In
--- Cabal expressions, for example, the BSD3 license would have to be
+-- licenses are Nix expressions — such as @lib.licenses.bsd3@ —,
+-- so their exact name is not generally known, because the path
+-- to @lib@ depends on the context defined in the expression.
+--
+-- In Cabal expressions, for example, the BSD3 license would have to be
 -- referred to as @self.lib.licenses.bsd3@. Other expressions,
--- however, use different paths to the @licenses@ record. Because of tat
+-- however, use different paths to the @licenses@ record. Because of that
 -- situation, the library cannot provide an abstract data type that
--- encompasses all known licenses. Instead, the @License@ type just
--- distinguishes references to known and unknown licenses. The
--- difference between the two is in the way they are pretty-printed:
+-- encompasses all known licenses.
 --
--- > > putStrLn (display (Known "lib.license.gpl2"))
--- > lib.license.gpl2
--- >
--- > > putStrLn (display (Unknown (Just "GPL")))
--- > "GPL"
--- >
--- > > putStrLn (display (Unknown Nothing))
--- > "unknown"
+-- Instead, the @License@ type just distinguishes references to known
+-- and unknown licenses. The difference between the two is in the way
+-- they are pretty-printed:
 --
--- Note that the "Text" instance definition provides pretty-printing,
+-- >>> putStrLn (prettyShow (Known "lib.license.gpl2"))
+-- lib.license.gpl2
+-- >>> putStrLn (prettyShow (Unknown (Just "GPL")))
+-- "GPL"
+-- >>> putStrLn (prettyShow (Unknown Nothing))
+-- "unknown"
+--
+-- Note that the 'Pretty' instance definition provides pretty-printing,
 -- but no parsing as of now!
 
 data License = Known String
