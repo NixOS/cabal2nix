3799564568295efff2a2c429fe81838015123af5
sternenseemann <sternenseemann@systemli.org>
2022-07-21 12:46:30 +0200

    Update build environment for subtree-ed dependencies
    * Add dependencies of hackage-db, languagae-nix and distribution-nixpkgs
      to shell environment, drop their nixpkgs versions.
    
    * Always build hackage-db examples, so they are not broken accidentally.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
   haskellPackages = pkgs.haskellPackages;
   ghc = haskellPackages.ghcWithHoogle (hps: [
     hps.ansi-wl-pprint
-    (hps.distribution-nixpkgs_1_7_0 or hps.distribution-nixpkgs)
-    hps.hackage-db
     hps.hopenssl
     hps.hpack
-    hps.language-nix
     hps.lens
     hps.optparse-applicative
     hps.pretty
@@ -18,14 +15,17 @@ let
     hps.monad-par-extras
     hps.tasty
     hps.tasty-golden
+    hps.utf8-string
+    hps.tar
+    hps.hspec
+    hps.parsec-class
   ]);
 
-in pkgs.stdenv.mkDerivation {
-  name = "shell";
-  buildInputs = [
-            ghc
-            haskellPackages.cabal-install
-            haskellPackages.haskell-language-server
-            (pkgs.lib.getLib pkgs.openssl)
-          ];
+in pkgs.mkShell {
+  packages = [
+    ghc
+    pkgs.cabal-install
+    haskellPackages.haskell-language-server
+    (pkgs.lib.getLib pkgs.openssl)
+  ];
 }
